ec0f624d26251f8f834b1cea5672c6f411469c58
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-06 14:55:42 -0300

    fix(entity): rename Name property to FullName in Employer class

--- src/Indica.System.Domain/Entities/Employer.cs
+++ src/Indica.System.Domain/Entities/Employer.cs
@@ -3,7 +3,7 @@ namespace Indica.System.Domain.Entities
 	public class Employer : EntityBase
 	{
 		public int Registry { get; set; }
-		public string Name { get; set; }
+		public string FullName { get; set; }
 		public DateOnly Admission { get; set; }
 		public DateOnly? Demission { get; set; }
 		public string Situation { get; set; }
