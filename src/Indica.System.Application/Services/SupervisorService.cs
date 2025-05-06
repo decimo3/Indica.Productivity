1dc2737b1701d61ff58630c9da473cbd93ca48d1
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-06 13:44:58 -0300

    feat(services): implement SupervisorService class inheriting from BaseService

---
+++ src/Indica.System.Application/Services/SupervisorService.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Indica.System.Application.DTO;
+using Indica.System.Application.Interfaces;
+using Indica.System.Domain.Entities;
+using Indica.System.Domain.Interfaces;
+
+namespace Indica.System.Application.Services
+{
+    public class SupervisorService : BaseService<SupervisorDTO, Supervisor>, ISupervisorService
+    {
+        public SupervisorService(ISupervisorRepository repository, IMapper mapper) : base(repository, mapper) { }
+    }
+}
