0d61119197fe8f63e533a418b61f281e92899ddf
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-06 08:15:04 -0300

    feat(infra): add database context

---
+++ src/Indica.System.Infra/ProductivityContext.cs
@@ -0,0 +1,12 @@
+﻿using Indica.System.Domain.Entities;
+using Indica.System.Infra.AutoMappers;
+using Microsoft.EntityFrameworkCore;
+
+namespace Indica.System.Infra
+{
+    public class ProductivityContext : DbContext
+    {
+        public ProductivityContext(DbContextOptions<ProductivityContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+    }
+}
