ba3717337d376360a89080251db228de595eeb0d
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-06 08:16:32 -0300

    feat(domain): add IBaseService interface for generic service operations

---
+++ src/Indica.System.Application/IBaseService.cs
@@ -0,0 +1,14 @@
+﻿
+using Indica.System.Domain.Entities;
+
+namespace Indica.System.Application
+{
+    public interface IBaseService<T, Y> where T : class where Y : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task<bool> AddAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(int id);
+    }
+}
