7a1905dee771b62de345e5737ccbe829fc9d940d
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-06 14:59:19 -0300

    feat(mapper): add ElectricianMapper for configuring Electrician entity

---
+++ src/Indica.System.Infra/AutoMappers/ElectricianMapper.cs
@@ -0,0 +1,32 @@
+﻿using Indica.System.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Indica.System.Infra.AutoMappers
+{
+    public class ElectricianMapper : IEntityTypeConfiguration<Electrician>
+    {
+        public void Configure(EntityTypeBuilder<Electrician> builder)
+        {
+            builder.ToTable("Eletricista");
+            builder.HasKey(e => e.Registry);
+            builder.Property(e => e.Registry)
+                .HasColumnName("matricula")
+                .IsRequired();
+            builder.Property(e => e.FullName)
+                .HasColumnName("nome_colaborador")
+                .IsRequired();
+            builder.Property(e => e.Admission)
+                .HasColumnName("adimissao")
+                .IsRequired();
+            builder.Property(e => e.Demission)
+                .HasColumnName("demissao");
+            builder.Property(e => e.Situation)
+                .HasColumnName("situacao")
+                .IsRequired();
+            builder.Property(e => e.IdSupervisor)
+                .HasColumnName("id_supervisor")
+                .IsRequired();
+        }
+    }
+}
