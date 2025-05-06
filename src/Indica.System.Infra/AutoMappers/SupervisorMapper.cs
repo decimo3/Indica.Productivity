65853c64bd15b6c67b98592c7ebaf266414bd68b
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-06 08:32:13 -0300

    feat(infra): add SupervisorMapper for Supervisor entity configuration

---
+++ src/Indica.System.Infra/AutoMappers/SupervisorMapper.cs
@@ -0,0 +1,32 @@
+﻿using Indica.System.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Indica.System.Infra.AutoMappers
+{
+    public class SupervisorMapper : IEntityTypeConfiguration<Supervisor>
+    {
+        public void Configure(EntityTypeBuilder<Supervisor> builder)
+        {
+            builder.ToTable("Supervisor");
+            builder.HasKey(e => e.Registry);
+            builder.Property(e => e.Registry)
+                .HasColumnName("matricula")
+                .IsRequired();
+            builder.Property(e => e.Name)
+                .HasColumnName("nome_colaborador")
+                .IsRequired();
+            builder.Property(e => e.Admission)
+                .HasColumnName("adimissao")
+                .IsRequired();
+            builder.Property(e => e.Demission)
+                .HasColumnName("demissao");
+            builder.Property(e => e.Situation)
+                .HasColumnName("situacao")
+                .IsRequired();
+            builder.Property(e => e.IdContract)
+                .HasColumnName("id_contrato")
+                .IsRequired();
+        }
+    }
+}
