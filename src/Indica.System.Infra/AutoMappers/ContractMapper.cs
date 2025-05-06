550c9c66996ef19df825dd0d62a443ec5dd43649
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-06 15:47:02 -0300

    feat(infra): add ContractMapper for Supervisor entity configuration

---
+++ src/Indica.System.Infra/AutoMappers/ContractMapper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Indica.System.Domain.Entities;
+
+namespace Indica.System.Infra.AutoMappers
+{
+    public class ContractMapper : IEntityTypeConfiguration<Contract>
+    {
+        public void Configure(EntityTypeBuilder<Contract> builder)
+        {
+            builder.ToTable("Contract");
+            builder.Property(c => c.IdContract)
+                .HasColumnName("identificador")
+                .IsRequired();
+            builder.Property(c => c.ContractNumber)
+                .HasColumnName("contrato")
+                .IsRequired();
+            builder.Property(c => c.AdditiveNumber)
+                .HasColumnName("aditivo")
+                .IsRequired();
+            builder.Property(c => c.StartDate)
+                .HasColumnName("inicio_vigencia")
+                .IsRequired();
+            builder.Property(c => c.FinalDate)
+                .HasColumnName("final_vigencia")
+                .IsRequired();
+
+        }
+    }
+}
