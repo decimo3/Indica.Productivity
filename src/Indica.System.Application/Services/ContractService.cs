07acbafeaff3a1a762e490ec4dff52cfa63094cf
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-06 16:02:49 -0300

    feat(services): implement ContractService for contract operations

---
+++ src/Indica.System.Application/Services/ContractService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Indica.System.Application.DTO;
+using Indica.System.Application.Interfaces;
+using Indica.System.Domain.Entities;
+using Indica.System.Domain.Interfaces;
+
+namespace Indica.System.Application.Services
+{
+    public class ContractService : BaseService<ContractDTO, Contract>, IContractService
+    {
+        public ContractService(IContractRepository repository, IMapper mapper) : base(repository, mapper)
+        {
+
+        }
+    }
+}
