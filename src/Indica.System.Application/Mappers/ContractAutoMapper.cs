ba5bac21e075b013e449422198939c166b199869
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-06 15:58:45 -0300

    feat(mappers): add ContractAutoMapper for mapping Contract to ContractDTO

---
+++ src/Indica.System.Application/Mappers/ContractAutoMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Indica.System.Application.DTO;
+using Indica.System.Domain.Entities;
+
+namespace Indica.System.Application.Mappers
+{
+    public class ContractAutoMapper : Profile
+    {
+        public ContractAutoMapper()
+        {
+            CreateMap<Contract, ContractDTO>().ReverseMap();
+        }
+    }
+}
