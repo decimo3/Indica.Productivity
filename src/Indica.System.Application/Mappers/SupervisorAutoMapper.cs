f8c7f4fcf03cbdd50e3c0e88db84443a5d46001c
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-06 14:38:23 -0300

    feat(mappers): add SupervisorAutoMapper for mapping SupervisorDTO to Supervisor

---
+++ src/Indica.System.Application/Mappers/SupervisorAutoMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Indica.System.Application.DTO;
+using Indica.System.Domain.Entities;
+
+namespace Indica.System.Application.Mappers
+{
+    public class SupervisorAutoMapper : Profile
+    {
+        public SupervisorAutoMapper()
+        {
+            CreateMap<SupervisorDTO, Supervisor>().ReverseMap();
+        }
+    }
+}
