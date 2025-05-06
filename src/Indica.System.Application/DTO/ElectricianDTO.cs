02ec3ffc09391c03a1f062f8a4e385eaa930a0c9
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-06 15:01:08 -0300

    feat(dto): add ElectricianDTO for Electrician entity representation

---
+++ src/Indica.System.Application/DTO/ElectricianDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Indica.System.Application.DTO
+{
+    public class ElectricianDTO : EmployerDTO
+    {
+        public int IdSupervisor { get; set; }
+        public string[] Habilities { get; set; }
+    }
+}
