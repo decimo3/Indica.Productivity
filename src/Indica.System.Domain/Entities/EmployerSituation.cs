a2569782be5084a099b9edc47dae861fb49b7999
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-14 17:34:21 -0300

    feat(entity): add EmployerSituation entity

---
+++ src/Indica.System.Domain/Entities/EmployerSituation.cs
@@ -0,0 +1,8 @@
+﻿namespace Indica.System.Domain.Entities
+{
+    public class EmployerSituation : EntityBase
+    {
+        public int IdSituation { get; set; }
+        public string SituationName { get; set; }
+    }
+}
