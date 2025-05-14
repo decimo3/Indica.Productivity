74559f1c18a485ed451d144d419fd1ee48e0bc2a
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-14 15:58:56 -0300

    feat(entity): add Role entity with properties for IdRole, RoleName, and Description

---
+++ src/Indica.System.Domain/Entities/Types/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace Indica.System.Domain.Entities.Types
+{
+    public class Role : EntityBase
+    {
+        public int IdRole { get; set; }
+        public string RoleName { get; set; }
+        public string Description { get; set; }
+    }
+}
