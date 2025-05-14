e26e36a2f45277bc35f73451d5ababb9af3a0f0e
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-14 14:59:13 -0300

    feat(service): implement EmployerService class for managing EmployerDTO and Employer

---
+++ src/Indica.System.Application/Services/EmployerService.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Indica.System.Application.DTO;
+using Indica.System.Application.Interfaces;
+using Indica.System.Domain.Entities;
+using Indica.System.Domain.Interfaces;
+
+namespace Indica.System.Application.Services
+{
+    public class EmployerService : BaseService<EmployerDTO, Employer>, IEmployerService
+    {
+        public EmployerService(IEmployerRepository repository, IMapper mapper) : base(repository, mapper) { }
+    }
+}
