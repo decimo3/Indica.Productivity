ac01e0754e01a87e3b968c09835e7b8062b7fdf0
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-14 17:09:28 -0300

    feat(mapper): add initial role data to RoleMapper for seeding roles in the database

--- src/Indica.System.Infra/EntityMappers/RoleMapper.cs
+++ src/Indica.System.Infra/EntityMappers/RoleMapper.cs
@@ -21,6 +21,44 @@ namespace Indica.System.Infra.EntityMappers
                 .HasColumnName("descricao")
                 .HasMaxLength(128)
                 .IsRequired();
+            builder.HasData([
+                new Role
+                {
+                    IdRole = 0,
+                    RoleName = "Eletricista",
+                    Description = "Responsável pela execução do serviço"
+                },
+                new Role
+                {
+                    IdRole = 1,
+                    RoleName = "Supervisor",
+                    Description = "Responsável pela gestão de equipes"
+                },
+                new Role
+                {
+                    IdRole = 2,
+                    RoleName = "Controlador",
+                    Description = "Responsável pelo suporte as equipes"
+                },
+                new Role
+                {
+                    IdRole = 3,
+                    RoleName = "Comunicador",
+                    Description = "Responsável pela gestão de qualidade"
+                },
+                new Role
+                {
+                    IdRole = 4,
+                    RoleName = "Administrador",
+                    Description = "Responsável pela gestão de supervisores e comunicadores"
+                },
+                new Role
+                {
+                    IdRole = 5,
+                    RoleName = "Proprietario",
+                    Description = "Responsável pela gestão de administradores"
+                },
+                ]);
         }
     }
 }
