cc1f3c61a6e917ade3f79b2ab2db46bb08097f49
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-14 14:57:06 -0300

    feat(mapper): add EmployerMapper configuration for Employer entity

---
+++ src/Indica.System.Infra/AutoMappers/EmployerMapper.cs
@@ -0,0 +1,32 @@
+﻿using Indica.System.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Indica.System.Infra.AutoMappers
+{
+    public class EmployerMapper : IEntityTypeConfiguration<Employer>
+    {
+        public void Configure(EntityTypeBuilder<Employer> builder)
+        {
+            builder.ToTable("funcionarios");
+            builder.HasKey(e => e.Registry);
+            builder.Property(e => e.Registry)
+                .HasColumnName("matricula")
+                .IsRequired();
+            builder.Property(e => e.FullName)
+                .HasColumnName("nome_colaborador")
+                .IsRequired();
+            builder.Property(e => e.Admission)
+                .HasColumnName("admissao")
+                .IsRequired();
+            builder.Property(e => e.Demission)
+                .HasColumnName("demissao");
+            builder.Property(e => e.IdRole)
+                .HasColumnName("id_cargo")
+                .IsRequired();
+            builder.Property(e => e.IdSituation)
+                .HasColumnName("id_situacao")
+                .IsRequired();
+        }
+    }
+}
