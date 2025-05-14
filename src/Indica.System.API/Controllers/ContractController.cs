442927486f1d30e4bcbf96d7144a61e73ed7db44
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-14 14:53:54 -0300

    refactor(controller): simplify ContractController by inheriting from GenericController

--- src/Indica.System.API/Controllers/ContractController.cs
+++ src/Indica.System.API/Controllers/ContractController.cs
@@ -1,51 +1,16 @@
 ﻿using Indica.System.Application.DTO;
 using Indica.System.Application.Interfaces;
+using Indica.System.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Indica.System.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ContractController : ControllerBase
+    public class ContractController : GenericController<ContractDTO, Contract>
     {
-        private readonly ILogger<ContractController> _logger;
-        private readonly IContractService _contractService;
-        public ContractController(IContractService contractService, ILogger<ContractController> logger)
+        public ContractController(IContractService service, ILogger<ContractController> logger) : base(service, logger)
         {
-            _logger = logger;
-            _contractService = contractService;
-        }
-        [HttpGet]
-        public async Task<IActionResult> Get()
-        {
-            var contracts = await _contractService.GetAllAsync();
-            return Ok(contracts);
-        }
-        [HttpGet]
-        [Route("{id}")]
-        public async Task<IActionResult> GetById(double id)
-        {
-            var contract = await _contractService.GetByIdAsync(id);
-            return Ok(contract);
-        }
-        [HttpPost]
-        public async Task<IActionResult> Post(ContractDTO contract)
-        {
-            await _contractService.AddAsync(contract);
-            return Created();
-        }
-        [HttpPut]
-        public async Task<IActionResult> Put(ContractDTO contract)
-        {
-            await _contractService.UpdateAsync(contract);
-            return NoContent();
-        }
-        [HttpDelete]
-        [Route("{id}")]
-        public async Task<IActionResult> Delete(double id)
-        {
-            await _contractService.DeleteAsync(id);
-            return NoContent();
         }
     }
 }
