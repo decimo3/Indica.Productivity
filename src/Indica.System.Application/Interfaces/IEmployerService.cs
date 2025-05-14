865483ec9276cdf0a7ed178d9fddde7ceb982dba
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-14 14:58:40 -0300

    feat(service): add IEmployerService interface for EmployerDTO and Employer

---
+++ src/Indica.System.Application/Interfaces/IEmployerService.cs
@@ -0,0 +1,7 @@
+﻿using Indica.System.Application.DTO;
+using Indica.System.Domain.Entities;
+
+namespace Indica.System.Application.Interfaces
+{
+    public interface IEmployerService : IBaseService<EmployerDTO, Employer> {}
+}
