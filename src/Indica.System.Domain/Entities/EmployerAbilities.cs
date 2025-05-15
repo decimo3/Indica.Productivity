559b8fe1fc2bb38475bff06a279499142655f693
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-15 15:44:56 -0300

    feat(entity): add EmployerAbilities class with IdEmployer and IdAbility properties

---
+++ src/Indica.System.Domain/Entities/EmployerAbilities.cs
@@ -0,0 +1,8 @@
+﻿namespace Indica.System.Domain.Entities
+{
+    public class EmployerAbilities
+    {
+        public int IdEmployer { get; set; }
+        public int IdAbility { get; set; }
+    }
+}
