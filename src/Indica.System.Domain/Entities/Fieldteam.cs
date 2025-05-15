0addfb247ba6a31e4274ef3178931c63cbf31842
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-15 15:45:21 -0300

    refactor(entity): update Fieldteam class by renaming OrderNumber to Order and removing unused properties

--- src/Indica.System.Domain/Entities/Fieldteam.cs
+++ src/Indica.System.Domain/Entities/Fieldteam.cs
@@ -4,17 +4,13 @@ namespace Indica.System.Domain.Entities
     {
         public string IdFieldteam { get; set; }
         public DateOnly Date { get; set; }
-        public int OrderNumber { get; set; }
+        public int Order { get; set; }
         public string Plate { get; set; }
         public string Resource { get; set; }
         public string ActivityName { get; set; }
-        public virtual Process Activity { get; set; }
-        public string IdCouple { get; set; }
-        public virtual Couple Couple { get; set; }
         public long Cellphone { get; set; }
         public string Region { get; set; }
         public bool IsSpecial { get; set; }
-        public double IdContract { get; set; }
-        public virtual Contract Contract { get; set; }
+        public bool IsConsidered { get; set; }
     }
 }
