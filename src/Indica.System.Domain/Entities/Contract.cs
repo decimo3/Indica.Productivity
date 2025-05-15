8e92595b81e65845249a3f5a4dee5968cf879533
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-15 15:11:47 -0300

    refactor(entity): Remove constructor from Contract class

--- src/Indica.System.Domain/Entities/Contract.cs
+++ src/Indica.System.Domain/Entities/Contract.cs
@@ -6,13 +6,5 @@ namespace Indica.System.Domain.Entities
         public int AdditiveNumber { get; set; }
         public DateOnly StartDate { get; set; }
         public DateOnly FinalDate { get; set; }
-        public Contract(long contractNumber, int additiveNumber, DateOnly startDate, DateOnly finalDate)
-        {
-            ContractNumber = contractNumber;
-            AdditiveNumber = additiveNumber;
-            StartDate = startDate;
-            FinalDate = finalDate;
-            IdContract = contractNumber + (additiveNumber * 0.01);
-        }
     }
 }
