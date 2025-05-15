7bad6d47636c42c0bed2abedd911430b0ee04e67
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-15 15:16:08 -0300

    refactor(entity): Remove unused properties from Process class

--- src/Indica.System.Domain/Entities/Process.cs
+++ src/Indica.System.Domain/Entities/Process.cs
@@ -1,12 +1,7 @@
-namespace Indica.System.Domain.Entities
+﻿namespace Indica.System.Domain.Entities
 {
     public class Process
     {
-        public string IdActivity { get; set; }
-        public string ActivityName { get; set; }
-        public bool IsBasketTruck { get; set; }
-        public bool IsHalfPrice { get; set; }
         public string ProcessName { get; set; }
-        public bool IsSpecial { get; set; }
     }
 }
