23e5bc06150234506700b80eaf646f3f73e29ba3
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-15 15:44:33 -0300

    feat(entity): add Activity class with properties for ActivityName, IdProcess, and flags

---
+++ src/Indica.System.Domain/Entities/Activity.cs
@@ -0,0 +1,11 @@
+namespace Indica.System.Domain.Entities
+{
+    public class Activity
+    {
+        public string ActivityName { get; set; }
+        public int IdProcess { get; set; }
+        public bool IsBasketTruck { get; set; }
+        public bool IsHalfPrice { get; set; }
+        public bool IsSpecial { get; set; }
+    }
+}
