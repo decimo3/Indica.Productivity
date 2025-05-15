8ba0c9825ed20019b09dc2b890f8f993c9e4fef1
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-15 15:29:30 -0300

    refactor(entity): renamed Description and removed PaymentMasters properties from Finishing class

--- src/Indica.System.Domain/Entities/Finishing.cs
+++ src/Indica.System.Domain/Entities/Finishing.cs
@@ -3,7 +3,6 @@ namespace Indica.System.Domain.Entities
     public class Finishing
     {
         public string GroupingOfMeasures { get; set; }
-        public string Description { get; set; }
-        public int[] PaymentMasters { get; set; }
+        public int IdFinishingDetail { get; set; }
     }    
 }
