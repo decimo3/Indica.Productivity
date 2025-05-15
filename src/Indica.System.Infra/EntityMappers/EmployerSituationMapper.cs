6422d0c8bb58e0b45020c046757ac0159a561436
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-15 08:29:32 -0300

    feat(mapper): Add EmployerSituationMapper class to configure EmployerSituation entity

---
+++ src/Indica.System.Infra/EntityMappers/EmployerSituationMapper.cs
@@ -0,0 +1,44 @@
+﻿using Indica.System.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Indica.System.Infra.EntityMappers
+{
+    public class EmployerSituationMapper : IEntityTypeConfiguration<EmployerSituation>
+    {
+        public void Configure(EntityTypeBuilder<EmployerSituation> builder)
+        {
+            builder.ToTable("EmployerSituations");
+            builder.HasKey(x => x.IdSituation);
+            builder.Property(x => x.IdSituation)
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+            builder.Property(x => x.SituationName)
+                .HasMaxLength(16)
+                .IsRequired();
+            builder.HasData([
+                new EmployerSituation
+                {
+                    IdSituation = 1,
+                    SituationName = "Ativo"
+                },
+                new EmployerSituation
+                {
+                    IdSituation = 2,
+                    SituationName = "Ferias"
+                },
+                new EmployerSituation
+                {
+                    IdSituation = 3,
+                    SituationName = "Afastado"
+                },
+                new EmployerSituation
+                {
+                    IdSituation = 4,
+                    SituationName = "Desligado"
+                },
+                ]);
+        }
+
+    }
+}
