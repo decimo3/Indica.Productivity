10db41264821ceab6ac4f85db2afd0939883cb25
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-15 15:15:47 -0300

    feat(entity): Add SupervisorContract class with Registry, ContractNumber, and AdditiveNumber properties

---
+++ src/Indica.System.Domain/Entities/SupervisorContract.cs
@@ -0,0 +1,9 @@
+﻿namespace Indica.System.Domain.Entities
+{
+    public class SupervisorContract : EntityBase
+    {
+        public int Registry { get; set; }
+        public int ContractNumber { get; set; }
+        public int AdditiveNumber { get; set; }
+    }
+}
