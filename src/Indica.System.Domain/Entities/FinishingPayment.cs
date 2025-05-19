45d6249cb27dd4cffad36841d42afa64ecc3f4e5
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-19 09:16:40 -0300

    feat(entity): add class FinishingPayment to hold relationship values between Finishing and Payment.

---
+++ src/Indica.System.Domain/Entities/FinishingPayment.cs
@@ -0,0 +1,8 @@
+﻿namespace Indica.System.Domain.Entities
+{
+    public class FinishingPayment : EntityBase
+    {
+        public string GroupingOfMeasures { get; set; }
+        public int IdPaymentMaster { get; set; }
+    }
+}
