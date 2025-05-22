19c61f206a56e1ff4aeeb22a74700b66b353f064
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-22 08:55:54 -0300

    feat(mapper): add FinishingMapper to configure Finishing entity

---
+++ src/Indica.System.Infra/EntityMappers/FinishingMapper.cs
@@ -0,0 +1,30 @@
+﻿using Indica.System.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Indica.System.Infra.EntityMappers
+{
+    public class FinishingMapper : IEntityTypeConfiguration<Finishing>
+    {
+        public void Configure(EntityTypeBuilder<Finishing> builder)
+        {
+            builder.ToTable("finalizacoes");
+            builder.HasKey(f => f.Id);
+            builder.Property(f => f.Id)
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+            builder.Property(f => f.GroupingOfMeasures)
+                .HasMaxLength(128)
+                .IsRequired();
+            builder.Property(f => f.IdFinishingDetail)
+                .IsRequired();
+            builder.HasOne<FinishingDetail>()
+                .WithMany()
+                .HasForeignKey(f => f.IdFinishingDetail)
+                .OnDelete(DeleteBehavior.SetNull);
+            builder.HasIndex(f => f.GroupingOfMeasures)
+                .IsUnique()
+                .HasDatabaseName("IX_Finishing_GroupingOfMeasures");
+        }
+    }
+}
