2ab9d16f23a1d6a2b92bde239dce927b967ac07f
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-22 08:58:25 -0300

    refactor(entity): Payment class
    - add inherit from EntityBase
    - replace Contract properties by IdContract
    - add entity navigation property for Contract

--- src/Indica.System.Domain/Entities/Payment.cs
+++ src/Indica.System.Domain/Entities/Payment.cs
@@ -1,11 +1,12 @@
 namespace Indica.System.Domain.Entities
 {
-    public class Payment
+    public class Payment : EntityBase
     {
-        public int ContractNumber { get; set; }
-        public int AdditiveNumber { get; set; }
+        public int IdContract { get; set; }
         public int IdProcess { get; set; }
         public int PaymentMaster { get; set; }
         public decimal Value { get; set; }
+        public Contract Contract { get; set; }
+        public Process Process { get; set; }
     }
 }
