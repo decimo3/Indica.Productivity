219dd8626b5752699cb7dd222e043e8e1c4bddb4
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-05-22 09:00:02 -0300

    feat(mapper): add FinishingPaymentMapper to configure FinishingPayment entity

---
+++ src/Indica.System.Infra/EntityMappers/FinishingPaymentMapper.cs
@@ -0,0 +1,33 @@
+﻿using Indica.System.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Indica.System.Infra.EntityMappers
+{
+    public class FinishingPaymentMapper : IEntityTypeConfiguration<FinishingPayment>
+    {
+        public void Configure(EntityTypeBuilder<FinishingPayment> builder)
+        {
+            builder.ToTable("finalizacao_pagamento");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+            builder.Property(x => x.GroupingOfMeasures)
+                .HasColumnName("agrupamento_medidas")
+                .HasMaxLength(128)
+                .IsRequired();
+            builder.Property(x => x.IdPaymentMaster)
+                .HasColumnName("id_mestre_pagamento")
+                .IsRequired();
+            builder.HasOne<Finishing>()
+                .WithMany()
+                .HasForeignKey(x => x.GroupingOfMeasures)
+                .OnDelete(DeleteBehavior.SetNull);
+            builder.HasOne<Payment>()
+                .WithMany()
+                .HasForeignKey(x => x.IdPaymentMaster)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
