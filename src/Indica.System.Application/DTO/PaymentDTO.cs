e9334be884878ace0fa29c5f7bd92c022bd520a6
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-06-11 15:49:37 -0300

    feat(dto): add PaymentDTO class with properties for payment details

---
+++ src/Indica.System.Application/DTO/PaymentDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Indica.System.Application.DTO
+{
+    public class PaymentDTO : EntityBaseDTO
+    {
+        public int IdContract { get; set; }
+        public int IdProcess { get; set; }
+        public int PaymentMaster { get; set; }
+        public decimal ValueLight { get; set; }
+        public decimal ValueHeavy { get; set; }
+        public decimal ValueSpecial { get; set; }
+    }
+}
