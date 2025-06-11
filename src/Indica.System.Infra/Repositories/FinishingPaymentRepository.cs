2b1620b883f3809d49b765f802889c376336e706
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-06-11 16:04:13 -0300

    feat(repository): implement FinishingPaymentRepository for FinishingPayment entity

---
+++ src/Indica.System.Infra/Repositories/FinishingPaymentRepository.cs
@@ -0,0 +1,10 @@
+﻿using Indica.System.Domain.Entities;
+using Indica.System.Domain.Interfaces;
+
+namespace Indica.System.Infra.Repositories
+{
+    public class FinishingPaymentRepository : BaseRepository<FinishingPayment>, IFinishingPaymentRepository
+    {
+        public FinishingPaymentRepository(ProductivityContext context) : base(context) { }
+    }
+}
