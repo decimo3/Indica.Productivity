1873b3eae74c32cf17de30d0d8711d3d2ae065e5
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-06-11 17:15:22 -0300

    fix(service): update GetByIdAsync and DeleteAsync methods to use int type for id parameter

--- src/Indica.System.Application/IBaseService.cs
+++ src/Indica.System.Application/IBaseService.cs
@@ -1,14 +1,13 @@
-﻿
-using Indica.System.Domain.Entities;
+﻿using Indica.System.Domain.Entities;
 
 namespace Indica.System.Application
 {
     public interface IBaseService<T, Y> where T : class where Y : class
     {
-        Task<T> GetByIdAsync(object id);
+        Task<T> GetByIdAsync(int id);
         Task<List<T>> GetAllAsync();
         Task<bool> AddAsync(T entity);
         Task<bool> UpdateAsync(T entity);
-        Task<bool> DeleteAsync(object id);
+        Task<bool> DeleteAsync(int id);
     }
 }
