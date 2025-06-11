1889df942a3863418e2d50eaa6886ba33d03d3fe
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-06-11 15:55:58 -0300

    feat(repository): implement FinishingRepository for Finishing entity

---
+++ src/Indica.System.Infra/Repositories/FinishingRepository.cs
@@ -0,0 +1,10 @@
+﻿using Indica.System.Domain.Entities;
+using Indica.System.Domain.Interfaces;
+
+namespace Indica.System.Infra.Repositories
+{
+    public class FinishingRepository : BaseRepository<Finishing>, IFinishingRepository
+    {
+        public FinishingRepository(ProductivityContext context) : base(context) { }
+    }
+}
