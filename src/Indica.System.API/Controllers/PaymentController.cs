e8417a9b192b91e9489d304f27ba1052bd381300
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-06-11 15:50:53 -0300

    feat(controller): add PaymentController for managing payment operations

---
+++ src/Indica.System.API/Controllers/PaymentController.cs
@@ -0,0 +1,11 @@
+﻿using Indica.System.Domain.Entities;
+using Indica.System.Application.DTO;
+using Indica.System.Application.Interfaces;
+
+namespace Indica.System.API.Controllers
+{
+    public class PaymentController : GenericController<PaymentDTO, Payment>
+    {
+        public PaymentController(IPaymentService service, ILogger<Payment> logger) : base(service, logger) {}
+    }
+}
