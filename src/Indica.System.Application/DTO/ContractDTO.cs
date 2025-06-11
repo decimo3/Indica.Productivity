d40b52f292de2ce55e57d0a91993cdc941e6bddc
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-06-11 15:38:55 -0300

    refactor(dto): remove unnecessary using directives from ContractDTO class

--- src/Indica.System.Application/DTO/ContractDTO.cs
+++ src/Indica.System.Application/DTO/ContractDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Indica.System.Application.DTO
+﻿namespace Indica.System.Application.DTO
 {
     public class ContractDTO : EntityBaseDTO
     {
