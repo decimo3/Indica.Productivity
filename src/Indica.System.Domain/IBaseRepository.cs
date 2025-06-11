1965aa588e123c7aa8443c4f9405079f7538386f
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-06-11 17:13:52 -0300

    fix(repository): update GetByIdAsync and DeleteAsync methods to use int type for id parameter

--- src/Indica.System.Domain/IBaseRepository.cs
+++ src/Indica.System.Domain/IBaseRepository.cs
@@ -1,13 +1,13 @@
-﻿using Indica.System.Domain.Entities;
+﻿using System.Linq.Expressions;
 
 namespace Indica.System.Domain
 {
     public interface IBaseRepository<T> where T : class
     {
-        Task<T> GetByIdAsync(object id);
+        Task<T> GetByIdAsync(int id);
         Task<List<T>> GetAllAsync();
         Task<bool> AddAsync(T entity);
         Task<bool> UpdateAsync(T entity);
-        Task<bool> DeleteAsync(object id);
+        Task<bool> DeleteAsync(int id);
     }
 }
