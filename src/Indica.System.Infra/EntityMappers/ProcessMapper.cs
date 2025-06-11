e20264ab241931e0f211aaff713dae9cb869e9c7
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-06-11 15:41:47 -0300

    feat(mapper): add ProcessMapper to configure Process entity

---
+++ src/Indica.System.Infra/EntityMappers/ProcessMapper.cs
@@ -0,0 +1,28 @@
+﻿using Indica.System.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Indica.System.Infra.EntityMappers
+{
+    public class ProcessMapper : IEntityTypeConfiguration<Process>
+    {
+        public void Configure(EntityTypeBuilder<Process> builder)
+        {
+            builder.ToTable("processos");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id)
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+            builder.Property(p => p.ProcessName)
+                .HasColumnName("nome")
+                .HasMaxLength(100)
+                .IsRequired();
+            builder.HasData([
+                new Process { Id = 1, ProcessName = "CORE" },
+                new Process { Id = 2, ProcessName = "LIDE" },
+                new Process { Id = 3, ProcessName = "REN" },
+                new Process { Id = 4, ProcessName = "ANEXO" },
+                ]);
+        }
+    }
+}
