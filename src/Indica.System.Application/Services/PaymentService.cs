2f79b2e8bcddbe2e1b09f17710eb3bdf41b4c44d
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-06-11 15:50:37 -0300

    feat(service): implement PaymentService class for handling payment operations

---
+++ src/Indica.System.Application/Services/PaymentService.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Indica.System.Application.DTO;
+using Indica.System.Application.Interfaces;
+using Indica.System.Domain.Entities;
+using Indica.System.Domain.Interfaces;
+
+namespace Indica.System.Application.Services
+{
+    public class PaymentService : BaseService<PaymentDTO, Payment>, IPaymentService
+    {
+        public PaymentService(IPaymentRepository paymentRepository, IMapper mapper) : base(paymentRepository, mapper)
+        {
+        }
+    }
+}
