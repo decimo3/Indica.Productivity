e05ad95bd711bf0e68d79850beb7ff3cbf00e29a
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-06-13 14:51:59 -0300

    feat(mapper): add CodeFilterMapper class for entity configuration

---
+++ src/Indica.System.Infra/EntityMappers/CodeFilterMapper.cs
@@ -0,0 +1,32 @@
+﻿using Indica.System.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Indica.System.Infra.EntityMappers
+{
+    public class CodeFilterMapper : IEntityTypeConfiguration<CodeFilter>
+    {
+        public void Configure(EntityTypeBuilder<CodeFilter> builder)
+        {
+            builder.ToTable("code_filter");
+            builder.HasKey(cf => cf.Id);
+            builder.Property(cf => cf.Id)
+                .HasColumnName("id_code_filter")
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+            builder.Property(cf => cf.Code)
+                .HasColumnName("code")
+                .HasMaxLength(4)
+                .IsRequired();
+            builder.Property(cf => cf.IdProcess)
+                .HasColumnName("id_process")
+                .IsRequired();
+            builder.HasOne<Process>()
+                .WithMany()
+                .HasForeignKey(cf => cf.IdProcess)
+                .OnDelete(DeleteBehavior.SetNull);
+            builder.HasIndex(cf => new { cf.Code, cf.IdProcess })
+                .IsUnique();
+        }
+    }
+}
