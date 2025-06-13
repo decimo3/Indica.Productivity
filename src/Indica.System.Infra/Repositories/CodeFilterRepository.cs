858a30137e498ecb00e59345bde8ce893763e92e
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-06-13 14:52:42 -0300

    feat(repository): implement CodeFilterRepository class for CodeFilter entity

---
+++ src/Indica.System.Infra/Repositories/CodeFilterRepository.cs
@@ -0,0 +1,10 @@
+﻿using Indica.System.Domain.Entities;
+using Indica.System.Domain.Interfaces;
+
+namespace Indica.System.Infra.Repositories
+{
+    public class CodeFilterRepository : BaseRepository<CodeFilter>, ICodeFilterRepository
+    {
+        public CodeFilterRepository(ProductivityContext context) : base(context) {}
+    }
+}
