ec4e96166295a671f7b9400c648d88313eb305ce
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-06-18 16:22:10 -0300

    feat(entity): add ActivityMapper for Activity entity configuration

---
+++ src/Indica.System.Infra/EntityMappers/ActivityMapper.cs
@@ -0,0 +1,42 @@
+﻿using Indica.System.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Indica.System.Infra.EntityMappers
+{
+    public class ActivityMapper : IEntityTypeConfiguration<Activity>
+    {
+        public void Configure(EntityTypeBuilder<Activity> builder)
+        {
+            builder.ToTable("atividade");
+            builder.HasKey(a => a.Id);
+            builder.Property(a => a.Id)
+                .HasColumnName("id_atividade")
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+            builder.Property(a => a.ActivityName)
+                .HasColumnName("nome_atividade")
+                .HasMaxLength(32)
+                .IsRequired();
+            builder.Property(a => a.IdProject)
+                .HasColumnName("id_projeto")
+                .IsRequired();
+            builder.Property(a => a.IsBasketTruck)
+                .HasColumnName("eh_caminhao")
+                .HasDefaultValue(false)
+                .IsRequired();
+            builder.Property(a => a.IsHalfPrice)
+                .HasColumnName("eh_metade")
+                .HasDefaultValue(false)
+                .IsRequired();
+            builder.Property(a => a.IsSpecial)
+                .HasColumnName("eh_especial")
+                .HasDefaultValue(false)
+                .IsRequired();
+            builder.HasOne<Project>()
+                .WithMany()
+                .HasForeignKey(a => a.IdProject)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
