da0104ff5f27f2fa37c8bb34f5c85eaca218538b
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-06-20 08:09:52 -0300

    feat(entity): add PaymentMaster class to represent payment details

---
+++ src/Indica.System.Domain/Entities/PaymentMaster.cs
@@ -0,0 +1,8 @@
+﻿namespace Indica.System.Domain.Entities
+{
+    public class PaymentMaster : EntityBase
+    {
+        public int Master { get; set; }
+        public string Description { get; set; }
+    }
+}
