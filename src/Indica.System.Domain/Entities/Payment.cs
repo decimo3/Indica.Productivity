ab3406ad44cde582cdf9e6d59c8f54fdba5841ce
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-06-20 08:06:45 -0300

    refactor(entity): rename PaymentMaster to IdPaymentMaster for consistency

--- src/Indica.System.Domain/Entities/Payment.cs
+++ src/Indica.System.Domain/Entities/Payment.cs
@@ -4,11 +4,9 @@ namespace Indica.System.Domain.Entities
     {
         public int IdContract { get; set; }
         public int IdProject { get; set; }
-        public int PaymentMaster { get; set; }
+        public int IdPaymentMaster { get; set; }
         public decimal ValueLight { get; set; }
         public decimal ValueHeavy { get; set; }
         public decimal ValueSpecial { get; set; }
-        public Contract Contract { get; set; }
-        public Process Process { get; set; }
     }
 }
