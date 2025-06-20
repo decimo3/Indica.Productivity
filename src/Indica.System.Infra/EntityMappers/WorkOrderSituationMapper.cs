1240e516937532e71cca3b79badff5d7417302ea
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-06-20 15:42:38 -0300

    feat(entity): add WorkOrderSituationMapper to configure WorkOrderSituation entity mapping in the database

---
+++ src/Indica.System.Infra/EntityMappers/WorkOrderSituationMapper.cs
@@ -0,0 +1,24 @@
+using Indica.System.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Indica.System.Infra.EntityMappers
+{
+    public class WorkOrderSituationMapper : IEntityTypeConfiguration<WorkOrderSituation>
+    {
+        public void Configure(EntityTypeBuilder<WorkOrderSituation> builder)
+        {
+            builder.ToTable("servico_situacao");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .HasColumnName("id_servico_situacao")
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+            builder.Property(x => x.SituationName)
+                .HasColumnName("nome_servico_situacao")
+                .HasMaxLength(32)
+                .IsRequired();
+            builder.HasIndex(x => x.SituationName).IsUnique();
+        }
+    }
+}
