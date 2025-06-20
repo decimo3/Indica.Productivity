4621549fa8d47690c2287dc3537bc1ec4bbbcb96
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-06-20 08:07:34 -0300

    refactor(entity): rename Registry properties in Employer class for clarity

--- src/Indica.System.Domain/Entities/Employer.cs
+++ src/Indica.System.Domain/Entities/Employer.cs
@@ -2,11 +2,12 @@ namespace Indica.System.Domain.Entities
 {
 	public class Employer : EntityBase
 	{
-		public int Registry { get; set; }
+        public int IndicaRegistry { get; set; }
+        public int ClientRegistry { get; set; }
 		public string FullName { get; set; }
 		public DateOnly Admission { get; set; }
 		public DateOnly? Demission { get; set; }
 		public int IdSituation { get; set; }
-        public int IdRole { get; set; }
+        public int IdFunction { get; set; }
     }
 }
