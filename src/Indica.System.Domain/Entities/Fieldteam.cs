9d95e6a702dd9fd5b4b60332c96eb0ca820d54cd
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-06-20 09:43:55 -0300

    refactor(entity): rename ActivityName to IdActivity and Region to IdRegion for improved clarity

--- src/Indica.System.Domain/Entities/Fieldteam.cs
+++ src/Indica.System.Domain/Entities/Fieldteam.cs
@@ -7,9 +7,9 @@ namespace Indica.System.Domain.Entities
         public int Order { get; set; }
         public string Plate { get; set; }
         public string Resource { get; set; }
-        public string ActivityName { get; set; }
+        public string IdActivity { get; set; }
         public long Cellphone { get; set; }
-        public string Region { get; set; }
+        public string IdRegion { get; set; }
         public bool IsSpecial { get; set; }
         public bool IsConsidered { get; set; }
     }
