bb855e7b1083ec5f47b1bba463b8aec02e36da1d
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-06-20 15:44:06 -0300

    feat(entity): add WorkOrderSituationMapper to configure WorkOrderSituation entity mapping in ProductivityContext

--- src/Indica.System.Infra/ProductivityContext.cs
+++ src/Indica.System.Infra/ProductivityContext.cs
@@ -24,6 +24,7 @@ namespace Indica.System.Infra
             modelBuilder.ApplyConfiguration(new FinishingPaymentMapper());
             modelBuilder.ApplyConfiguration(new CodeFilterMapper());
             modelBuilder.ApplyConfiguration(new FieldTeamRegionalMapper());
+            modelBuilder.ApplyConfiguration(new WorkOrderSituationMapper());
         }
     }
 }
