a309e45130d74e793da1c55ea2f875bbaa0fb7a9
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-06-24 09:46:14 -0300

    feat(entity): add FieldTeamMapper configuration for Fieldteam entity mapping in the database

---
+++ src/Indica.System.Infra/EntityMappers/FieldTeamMapper.cs
@@ -0,0 +1,63 @@
+using Indica.System.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Indica.System.Infra.EntityMappers
+{
+    public class FieldTeamMapper : IEntityTypeConfiguration<Fieldteam>
+    {
+        public void Configure(EntityTypeBuilder<Fieldteam> builder)
+        {
+            builder.ToTable("composicao");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .HasColumnName("id_composicao")
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+            builder.Property(x => x.Date)
+                .HasColumnName("dia")
+                .IsRequired();
+            builder.Property(x => x.Resource)
+                .HasColumnName("recurso")
+                .IsRequired();
+            builder.Property(x => x.Order)
+                .HasColumnName("ordem")
+                .IsRequired();
+            builder.Property(x => x.Plate)
+                .HasColumnName("placa")
+                .IsFixedLength()
+                .HasMaxLength(7)
+                .IsRequired();
+            builder.Property(x => x.Cellphone)
+                .HasColumnName("telefone")
+                .IsRequired();
+            builder.Property(x => x.IdFieldteam)
+                .HasColumnName("abreviatura")
+                .IsRequired();
+            builder.Property(x => x.IdActivity)
+                .HasColumnName("id_atividade")
+                .IsRequired();
+            builder.Property(x => x.IdRegion)
+                .HasColumnName("id_regional")
+                .IsRequired();
+            builder.Property(x => x.IsConsidered)
+                .HasColumnName("eh_considerado")
+                .HasDefaultValue(true)
+                .IsRequired();
+            builder.Property(x => x.IsSpecial)
+                .HasColumnName("eh_especial")
+                .HasDefaultValue(false)
+                .IsRequired();
+            builder.HasOne<Activity>()
+                .WithMany()
+                .HasForeignKey(x => x.IdActivity)
+                .OnDelete(DeleteBehavior.SetNull)
+                .IsRequired();
+            builder.HasOne<Regional>()
+                .WithMany()
+                .HasForeignKey(x => x.IdRegion)
+                .OnDelete(DeleteBehavior.SetNull)
+                .IsRequired();
+        }
+    }
+}
