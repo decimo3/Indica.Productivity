868348f543486e1fb6c02a151742950fb339daf4
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-06-26 15:05:08 -0300

    feat(dto): add WorkOrderDTO as a placeholder for work order data representation

---
+++ src/Indica.System.Application/DTO/WorkOrderDTO.cs
@@ -0,0 +1,10 @@
+namespace Indica.System.Application.DTO
+{
+    public class WorkOrderDTO : EntityBaseDTO
+    {
+        public WorkOrderDTO()
+        {
+            throw new NotImplementedException("Do not use this class");
+        }
+    }
+}
