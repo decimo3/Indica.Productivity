8d2417572dc4a8413906092d35a4c4169bc83810
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-07-01 15:04:08 -0300

    feat(entity): add WorkOrderShiftInfo entity to represent shift details for work orders

---
+++ src/Indica.System.Domain/Entities/WorkOrderShiftInfo.cs
@@ -0,0 +1,13 @@
+namespace Indica.System.Domain.Entities
+{
+    public class WorkOrderShiftInfo : WorkOrderBase
+    {
+        public DateTime ShiftStartDate { get; set; }
+        public string VehicleLabel { get; set; }
+        public int IdLeaderRegistration { get; set; }
+        public int IdAuxiliaryRegistration { get; set; }
+        public int IdTechnicalRegistration { get; set; }
+        public string ReasonForTimeInterval { get; set; }
+        public string? Observation { get; set; }
+    }
+}
