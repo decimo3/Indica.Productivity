4cb18fe3e4184ebba310a00368f2e9a5d7bf3797
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-07-01 15:04:22 -0300

    feat(entity): add WorkOrderService entity to manage work order details and associated properties

---
+++ src/Indica.System.Domain/Entities/WorkOrderService.cs
@@ -0,0 +1,35 @@
+namespace Indica.System.Domain.Entities
+{
+    public class WorkOrderService : WorkOrderBase
+    {
+        public long WorkOrderNumber { get; set; }
+        public int IdCostumer { get; set; }
+        public int IdWorkAbility { get; set; }
+        public DateTime StartOfSLA { get; set; }
+        public DateTime FinalOfSLA { get; set; }
+        public string ClosingCodes { get; set; }
+        public bool IsLgCtrlTypeClosingOk { get; set; }
+        public bool IsClosedCodesFilledIn { get; set; }
+        public string ClosingCodes_1 { get; set; }
+        public string? Observation { get; set; }
+        public string? Description { get; set; }
+        public bool IsLgFlagPrefillimentoClosing { get; set; }
+        public string ParentActivityClosingCodesV03 { get; set; }
+        public bool IsLgCtrlReprovedFlag { get; set; }
+        public long InstallationNumber { get; set; }
+        public string TypeOfServiceNote { get; set; }
+        public decimal TotalCustomerDebts { get; set; }
+        public bool HasCustomerSignedToi { get; set; }
+        public bool HasRefusedToSignToi { get; set; }
+        public bool HasRefusedToReceiveToi { get; set; }
+        public string CustomerAuthorizedloadAnalysis { get; set; }
+        public string ScopeOfService { get; set; }
+        public int CHI { get; set; }
+        public int InterruptedTime { get; set; }
+        public int FinancialCompensationAmount { get; set; }
+        public int IdFinishing { get; set; }
+        public virtual Finishing? Finishing { get; set; }
+        public virtual WorkOrderCostumer? WorkOrderCostumer { get; set; }
+        public virtual WorkOrderAbilities? WorkOrderAbility { get; set; }
+    }
+}
