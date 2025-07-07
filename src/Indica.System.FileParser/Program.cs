76ecfa0bbf2c78f1d59532a6e363a2867769666c
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-07-07 14:56:58 -0300

    feat(fileparser): implement XLSX parsing functionality in ParseXLSX class for data extraction

---
+++ src/Indica.System.FileParser/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Data;
+using System.Reflection;
+using ExcelDataReader;
+namespace Indica.System.FileParser
+{
+    public static class ParseXLSX
+    {
+        public static List<T> ParseByFilepath<T>(string filepath, string sheetname) where T : new()
+        {
+            if (!File.Exists(filepath))
+            {
+                throw new InvalidOperationException();
+            }
+            if (!string.Equals(Path.GetExtension(filepath), ".xlsx"))
+            {
+                throw new InvalidOperationException();
+            }
+            using var stream = File.Open(filepath, FileMode.Open, FileAccess.Read);
+            using var reader = ExcelReaderFactory.CreateReader(stream);
+            var datatable = reader.AsDataSet().Tables[sheetname];
+            if (datatable is null)
+            {
+                throw new InvalidOperationException("Planilha não encontrada!");
+            }
+            var type = typeof(T);
+            var properties = type.GetProperties(BindingFlags.Public);
+            var list = new List<T>();
+            foreach (DataRow row in datatable.Rows)
+            {
+                T item = new();
+                foreach (var property in properties)
+                {
+                    var value = row[property.Name];
+                    if (value is null) continue;
+                    var converted = Convert.ChangeType(value, property.GetType(), null);
+                    property.SetValue(item, converted);
+                }
+                list.Add(item);
+            }
+            return list;
+        }
+    }
+}
