91e26868ee857f136c102a6ecb61187c307f9cda
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-07-09 10:42:38 -0300

    feat(entity): add WorkOrderInterval entity to represent unavailable reasons or interval descriptions

---
+++ src/Indica.System.Domain/Entities/WorkOrderInterval.cs
@@ -0,0 +1,7 @@
+namespace Indica.System.Domain.Entities
+{
+    public class WorkOrderInterval : WorkOrderBase
+    {
+        public string UnavailableReasonOrIntervalDescription { get; set; }
+    }
+}
