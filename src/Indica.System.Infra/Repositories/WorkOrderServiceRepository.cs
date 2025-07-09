33803ab8cce1921046aabcb0e7e126a1c978b1f6
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-07-09 11:19:53 -0300

    refactor(repository): update WorkOrderServiceRepository constructor to use WorkOrderService type

--- src/Indica.System.Infra/Repositories/WorkOrderServiceRepository.cs
+++ src/Indica.System.Infra/Repositories/WorkOrderServiceRepository.cs
@@ -3,8 +3,8 @@ using Indica.System.Domain.Interfaces;
 
 namespace Indica.System.Infra.Repositories
 {
-    public class WorkOrderRepository : BaseRepository<WorkOrder>, IWorkOrderRepository
+    public class WorkOrderServiceRepository : BaseRepository<WorkOrderService>, IWorkOrderServiceRepository
     {
-        public WorkOrderRepository(ProductivityContext context) : base(context) {}
+        public WorkOrderServiceRepository(ProductivityContext context) : base(context) {}
     }
 }
