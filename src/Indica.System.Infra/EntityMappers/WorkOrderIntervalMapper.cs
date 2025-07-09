0e8fbf957a048b6c5ea8bd73f404011f96775e13
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-07-09 10:43:00 -0300

    feat(entitymapper): add WorkOrderIntervalMapper for mapping WorkOrderInterval entity to database table

---
+++ src/Indica.System.Infra/EntityMappers/WorkOrderIntervalMapper.cs
@@ -0,0 +1,20 @@
+using Indica.System.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Indica.System.Infra.EntityMappers
+{
+    public class WorkOrderIntervalMapper : IEntityTypeConfiguration<WorkOrderInterval>
+    {
+        public void Configure(EntityTypeBuilder<WorkOrderInterval> builder)
+        {
+            builder.ToTable("servico_intervalo");
+            builder.UseTpcMappingStrategy();
+            WorkOrderMapperHelper.ConfigureBase(builder);
+            builder.Property(x => x.UnavailableReasonOrIntervalDescription)
+                .HasColumnName("motivo_indisponibilidade_ou_descricao_intervalo")
+                .HasMaxLength(32)
+                .IsRequired();
+        }
+    }
+}
