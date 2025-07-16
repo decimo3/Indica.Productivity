1c31d88d1c65c276ce5fc9c1e6ab18848158eea8
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-07-16 09:01:47 -0300

    refactor(entity): reorganize model configurations in ProductivityContext to improve clarity and maintainability

--- src/Indica.System.Infra/ProductivityContext.cs
+++ src/Indica.System.Infra/ProductivityContext.cs
@@ -8,29 +8,41 @@ namespace Indica.System.Infra
         public ProductivityContext(DbContextOptions<ProductivityContext> options) : base(options) { }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.ApplyConfiguration(new EmployerMapper());
-            modelBuilder.ApplyConfiguration(new ContractMapper());
-            modelBuilder.ApplyConfiguration(new EmployerAbilityMapper());
-            modelBuilder.ApplyConfiguration(new EmployerFunctionMapper());
-            modelBuilder.ApplyConfiguration(new EmployerSituationMapper());
             modelBuilder.ApplyConfiguration(new ProcessMapper());
             modelBuilder.ApplyConfiguration(new ProjectMapper());
             modelBuilder.ApplyConfiguration(new ActivityMapper());
-            modelBuilder.ApplyConfiguration(new PaymentMasterMapper());
-            modelBuilder.ApplyConfiguration(new PaymentMapper());
-            modelBuilder.ApplyConfiguration(new FinishingDetailMapper());
-            modelBuilder.ApplyConfiguration(new FinishingMapper());
-            modelBuilder.ApplyConfiguration(new FinishingPaymentMapper());
-            modelBuilder.ApplyConfiguration(new CodeFilterMapper());
+
+            modelBuilder.ApplyConfiguration(new ContractMapper());
+            modelBuilder.ApplyConfiguration(new ObjectiveMapper());
+
+            modelBuilder.ApplyConfiguration(new EmployerFunctionMapper());
+            modelBuilder.ApplyConfiguration(new EmployerSituationMapper());
+            //modelBuilder.ApplyConfiguration(new EmployerAbilityMapper());
+            //modelBuilder.ApplyConfiguration(new EmployerAbilitiesMapper());
+            modelBuilder.ApplyConfiguration(new EmployerMapper());
+
             modelBuilder.ApplyConfiguration(new FieldTeamRegionalMapper());
             modelBuilder.ApplyConfiguration(new FieldTeamFuncionMapper());
-            modelBuilder.ApplyConfiguration(new WorkOrderSituationMapper());
-            modelBuilder.ApplyConfiguration(new ObjectiveMapper());
-            modelBuilder.ApplyConfiguration(new DamageToProcessMapper());
+            modelBuilder.ApplyConfiguration(new FieldTeamCoupleMapper());
             modelBuilder.ApplyConfiguration(new FieldTeamMapper());
-            modelBuilder.ApplyConfiguration(new CoupleMapper());
-            modelBuilder.ApplyConfiguration(new EmployerAbilitiesMapper());
-            modelBuilder.ApplyConfiguration(new WorkOrderMapper());
+
+            modelBuilder.ApplyConfiguration(new DamageToProcessMapper());
+            modelBuilder.ApplyConfiguration(new CodeFilterMapper());
+
+            modelBuilder.ApplyConfiguration(new FinishingDetailMapper());
+            modelBuilder.ApplyConfiguration(new FinishingMapper());
+
+            modelBuilder.ApplyConfiguration(new PaymentMasterMapper());
+            modelBuilder.ApplyConfiguration(new PaymentMapper());
+
+            modelBuilder.ApplyConfiguration(new FinishingPaymentMapper());
+
+            modelBuilder.ApplyConfiguration(new WorkOrderSituationMapper());
+            modelBuilder.ApplyConfiguration(new WorkOrderAbilitiesMapper());
+            modelBuilder.ApplyConfiguration(new WorkOrderIntervalMapper());
+            modelBuilder.ApplyConfiguration(new WorkOrderShiftInfoMapper());
+            modelBuilder.ApplyConfiguration(new WorkOrderCostumerMapper());
+            modelBuilder.ApplyConfiguration(new WorkOrderServiceMapper());
         }
     }
 }
