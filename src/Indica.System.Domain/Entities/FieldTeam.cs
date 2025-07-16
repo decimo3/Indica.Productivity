71e5911b5ba8fef6b93e01ebff79572baf76afd7
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-07-16 17:47:01 -0300

    refactor(entity): change IdActivity and IdRegion properties in FieldTeam class from string to int for type consistency

--- src/Indica.System.Domain/Entities/FieldTeam.cs
+++ src/Indica.System.Domain/Entities/FieldTeam.cs
@@ -7,9 +7,9 @@ namespace Indica.System.Domain.Entities
         public int Order { get; set; }
         public string Plate { get; set; }
         public string Resource { get; set; }
-        public string IdActivity { get; set; }
+        public int IdActivity { get; set; }
         public long Cellphone { get; set; }
-        public string IdRegion { get; set; }
+        public int IdRegion { get; set; }
         public bool IsSpecial { get; set; }
         public bool IsConsidered { get; set; }
     }
