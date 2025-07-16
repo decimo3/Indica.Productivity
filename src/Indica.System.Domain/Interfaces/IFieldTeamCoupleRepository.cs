0d04b8fbc8740f54305d7bbce61d0fde9ae2667a
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-07-16 09:01:04 -0300

    refactor(repository): rename ICoupleRepository to IFieldTeamCoupleRepository for consistency with updated entity name

--- src/Indica.System.Domain/Interfaces/IFieldTeamCoupleRepository.cs
+++ src/Indica.System.Domain/Interfaces/IFieldTeamCoupleRepository.cs
@@ -2,5 +2,5 @@ using Indica.System.Domain.Entities;
 
 namespace Indica.System.Domain.Interfaces
 {
-    public interface ICoupleRepository : IBaseRepository<Couple> { }
+    public interface IFieldTeamCoupleRepository : IBaseRepository<FieldTeamCouple> { }
 }
