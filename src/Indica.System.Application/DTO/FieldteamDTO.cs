f6cf0e39a2eea0473187de2f34cb64396051050f
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-07-22 17:53:01 -0300

    feat(dto): add new properties with aliases to FieldTeamDTO for enhanced data mapping

--- src/Indica.System.Application/DTO/FieldteamDTO.cs
+++ src/Indica.System.Application/DTO/FieldteamDTO.cs
@@ -1,20 +1,34 @@
+using Indica.System.Shared;
 namespace Indica.System.Application.DTO
 {
     public class FieldTeamDTO : EntityBaseDTO
     {
         public string IdFieldTeam { get; set; }
+        [Alias("DATA")]
         public DateOnly Date { get; set; }
+        [Alias("ADESIVO LIGHT")]
         public int Order { get; set; }
+        [Alias("PLACA")]
         public string Plate { get; set; }
+        [Alias("RECURSO")]
         public string Resource { get; set; }
+        [Alias("ATIVIDADE")]
         public string ActivityName { get; set; }
+        [Alias("MATRÍCULA 01")]
         public int EmployerRegistry1 { get; set; }
+        [Alias("EXECUTOR 01")]
         public string EmployerName1 { get; set; }
+        [Alias("MATRÍCULA 02")]
         public int EmployerRegistry2 { get; set; }
+        [Alias("EXECUTOR 02")]
         public string EmployerName2 { get; set; }
+        [Alias("TELEFONE EQUIPE")]
         public long Cellphone { get; set; }
+        [Alias("MATRICULA SUP")]
         public int SupervisorRegistry { get; set; }
+        [Alias("SUPERVISOR EMPREITEIRA")]
         public string SupervisorName { get; set; }
+        [Alias("ÁREA DE ATUAÇÃO")]
         public string WorkArea { get; set; }
     }
 }
