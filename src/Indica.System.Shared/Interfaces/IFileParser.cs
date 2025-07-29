ee5aa654c3c78b5de79ed32e64de0f960662c630
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-07-29 11:46:37 -0300

    refactor(IFileParser): add overload for ParseByFilepath to accept Stream and filename

--- src/Indica.System.Shared/Interfaces/IFileParser.cs
+++ src/Indica.System.Shared/Interfaces/IFileParser.cs
@@ -3,5 +3,6 @@
     public interface IFileParser
     {
         public List<T> ParseByFilepath<T>(string filepath) where T : new();
+        public List<T> ParseByFilepath<T>(Stream stream, string filename) where T : new();
     }
 }
