c2be249aec6dafbe1f9ebf3cd36cebf2b8d40669
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-07-29 17:43:17 -0300

    feat(DTO): add ProjectDTO class with ProjectName and IdProcess properties

---
+++ src/Indica.System.Application/DTO/ProjectDTO.cs
@@ -0,0 +1,8 @@
+namespace Indica.System.Application.DTO
+{
+    public class ProjectDTO : EntityBaseDTO
+    {
+        public string ProjectName { get; set; }
+        public int IdProcess { get; set; }
+    }
+}
