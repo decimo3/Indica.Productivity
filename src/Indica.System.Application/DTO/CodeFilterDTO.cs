fef8ef6ff9a8fb0cbe4b8fa499e6b391efe3a2e8
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-07-29 17:40:39 -0300

    feat(DTO): add CodeFilterDTO class with properties for code and project ID

---
+++ src/Indica.System.Application/DTO/CodeFilterDTO.cs
@@ -0,0 +1,8 @@
+namespace Indica.System.Application.DTO
+{
+    public class CodeFilterDTO : EntityBaseDTO
+    {
+        public string Code { get; set; }
+        public int IdProject { get; set; }
+    }
+}
