e00e1e2370f48f46769739053a701c5bbb66af0e
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-07-30 09:01:06 -0300

    feat(service): add IProjectService interface for ProjectDTO and Project

---
+++ src/Indica.System.Application/Interfaces/IProjectService.cs
@@ -0,0 +1,9 @@
+﻿using Indica.System.Application.DTO;
+using Indica.System.Domain.Entities;
+
+namespace Indica.System.Application.Interfaces
+{
+    public interface IProjectService : IBaseService<ProjectDTO, Project>
+    {
+    }
+}
