16bf5769ecf6b9b6e6b3bc8934c5cce5622e4568
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-07-30 08:59:26 -0300

    feat(service): add IDamageToProjectService interface for DamageToProjectDTO and DamageToProcess

---
+++ src/Indica.System.Application/Interfaces/IDamageToProjectService.cs
@@ -0,0 +1,9 @@
+﻿using Indica.System.Application.DTO;
+using Indica.System.Domain.Entities;
+
+namespace Indica.System.Application.Interfaces
+{
+    public interface IDamageToProjectService : IBaseService<DamageToProjectDTO, DamageToProcess>
+    {
+    }
+}
