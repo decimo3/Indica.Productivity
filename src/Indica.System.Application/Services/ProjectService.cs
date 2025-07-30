8f4c1df215637c18c625dde4e232ddf7e8ed9837
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-07-30 15:38:13 -0300

    feat(service): add ProjectService implementation

---
+++ src/Indica.System.Application/Services/ProjectService.cs
@@ -0,0 +1,17 @@
+using AutoMapper;
+using Indica.System.Application.DTO;
+using Indica.System.Application.Interfaces;
+using Indica.System.Domain.Entities;
+using Indica.System.Domain.Interfaces;
+using Indica.System.Shared.Interfaces;
+
+namespace Indica.System.Application.Services
+{
+    public class ProjectService : BaseService<ProjectDTO, Project>, IProjectService
+    {
+        public ProjectService(IProjectRepository repository, IMapper mapper, IFileParser parser) : base(repository, mapper, parser)
+        {
+
+        }
+    }
+}
