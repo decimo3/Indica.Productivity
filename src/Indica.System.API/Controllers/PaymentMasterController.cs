d86a63aab800ecf4718baeae68949d0ee8942fbb
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-07-31 16:43:06 -0300

    feat(controller): add controller to PaymentMaster entity

---
+++ src/Indica.System.API/Controllers/PaymentMasterController.cs
@@ -0,0 +1,16 @@
+using Indica.System.Application.DTO;
+using Indica.System.Application.Interfaces;
+using Indica.System.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Indica.System.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PaymentMasterController : GenericController<PaymentMasterDTO, PaymentMaster>
+    {
+        public PaymentMasterController(IPaymentMasterService service, ILogger<PaymentMasterController> logger) : base(service, logger)
+        {
+        }
+    }
+}
