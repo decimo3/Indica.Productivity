fc1f45582fed85daf82391ad64e3d235326fbdc4
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-07-31 16:42:24 -0300

    feat(controller): add controller to DamageToProject entity

---
+++ src/Indica.System.API/Controllers/DamageToProjectController.cs
@@ -0,0 +1,16 @@
+using Indica.System.Application.DTO;
+using Indica.System.Application.Interfaces;
+using Indica.System.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Indica.System.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DamageToProjectController : GenericController<DamageToProjectDTO, DamageToProcess>
+    {
+        public DamageToProjectController(IDamageToProjectService service, ILogger<DamageToProjectController> logger) : base(service, logger)
+        {
+        }
+    }
+}
