5e5abfed86fd76f3bab4b510bb62b9af8495794d
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-05 14:24:44 -0300

    chore(database): fix table name on foreight key reference

--- database.sql
+++ database.sql
@@ -210,7 +210,7 @@ INSERT INTO finalizacao_categorias (id_finalizacao_categoria, nome_finalizacao_c
 CREATE TABLE IF NOT EXISTS finalizacoes (
     id_finalizacao INTEGER PRIMARY KEY,
     agrupamento_medidas VARCHAR(128) NOT NULL,
-    id_categoria INTEGER REFERENCES categorias(id_categoria)
+    id_categoria INTEGER REFERENCES finalizacao_categorias(id_finalizacao_categoria)
 );
 
 CREATE TABLE IF NOT EXISTS finalizacoes_pagamento (
