eb58341cb1df6815524a37ac0bded9e21fa38c12
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-05 16:18:31 -0300

    refactor(entity): remove unused property on FieldTeam class

--- src/Indica.System.Domain/Entities/FieldTeam.cs
+++ src/Indica.System.Domain/Entities/FieldTeam.cs
@@ -9,7 +9,6 @@ namespace Indica.System.Domain.Entities
         public int IdActivity { get; set; }
         public long Cellphone { get; set; }
         public int IdRegion { get; set; }
-        public bool IsSpecial { get; set; }
         public bool IsConsidered { get; set; }
         public Activity Activity { get; set; }
         public FieldTeamRegional Regional { get; set; }
