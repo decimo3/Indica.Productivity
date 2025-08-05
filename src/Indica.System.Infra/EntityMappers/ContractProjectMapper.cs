0a79284a9d31c64d350eb38503841310a0115b0e
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-05 16:30:17 -0300

    feat(mapper): add mapper to ContractProject entity

---
+++ src/Indica.System.Infra/EntityMappers/ContractProjectMapper.cs
@@ -0,0 +1,43 @@
+using Indica.System.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Indica.System.Infra.EntityMappers
+{
+    public class ContractProjectMapper : IEntityTypeConfiguration<ContractProject>
+    {
+        public void Configure(EntityTypeBuilder<ContractProject> builder)
+        {
+            builder.ToTable("contrato_projeto");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .HasColumnName("id_contrato_projeto")
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+            builder.Property(x => x.IdContract)
+                .HasColumnName("id_contrato")
+                .IsRequired();
+            builder.Property(x => x.IdProject)
+                .HasColumnName("id_projeto")
+                .IsRequired();
+            builder.Property(x => x.IdRegional)
+                .HasColumnName("id_regional")
+                .IsRequired();
+            builder.HasOne(x => x.Contract)
+                .WithMany()
+                .HasForeignKey(x => x.IdContract)
+                .OnDelete(DeleteBehavior.SetNull)
+                .IsRequired();
+            builder.HasOne(x => x.Project)
+                .WithMany()
+                .HasForeignKey(x => x.IdProject)
+                .OnDelete(DeleteBehavior.SetNull)
+                .IsRequired();
+            builder.HasOne(x => x.Regional)
+                .WithMany()
+                .HasForeignKey(x => x.IdRegional)
+                .OnDelete(DeleteBehavior.SetNull)
+                .IsRequired();
+        }
+    }
+}
