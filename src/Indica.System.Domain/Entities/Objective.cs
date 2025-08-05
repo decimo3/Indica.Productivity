238b9532c4998b9ec4958fb37f7dd3be7eebf5f7
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-05 17:17:20 -0300

    refactor(entity): remove unused property `IsEspecial` on Objective class

--- src/Indica.System.Domain/Entities/Objective.cs
+++ src/Indica.System.Domain/Entities/Objective.cs
@@ -6,7 +6,6 @@ namespace Indica.System.Domain.Entities
         public int IdProcess { get; set; }
         public bool IsBasketTruck { get; set; }
         public bool IsHalfPrice { get; set; }
-        public bool IsEspecial { get; set; }
         public decimal MonthlyProfitGoal { get; set; }
         public float FixedDivisorByMonth { get; set; }
         public int TargetOfTeamCountOnWorkday { get; set; }
