3b39e66e0b8dcd6570a52267bafa9d7d5efc55d3
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-05 17:34:57 -0300

    feat(mapper): add mapper to configure WorkOrderPhase entity

---
+++ src/Indica.System.Infra/EntityMappers/WorkOrderPhaseMapper.cs
@@ -0,0 +1,29 @@
+using Indica.System.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Indica.System.Infra.EntityMappers
+{
+    public class WorkOrderPhaseMapper : IEntityTypeConfiguration<WorkOrderPhase>
+    {
+        public void Configure(EntityTypeBuilder<WorkOrderPhase> builder)
+        {
+            builder.ToTable("servico_fases");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .HasColumnName("id_servico_fase")
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+            builder.Property(x => x.PhaseName)
+                .HasColumnName("nome_servico_fase")
+                .HasMaxLength(16)
+                .IsRequired();
+            builder.HasIndex(x => x.PhaseName).IsUnique();
+            builder.HasData([
+                new WorkOrderPhase() { Id = 1, PhaseName = "Monofásico" },
+                new WorkOrderPhase() { Id = 2, PhaseName = "Bifásico" },
+                new WorkOrderPhase() { Id = 3, PhaseName = "Trifásico" },
+            ]);
+        }
+    }
+}
