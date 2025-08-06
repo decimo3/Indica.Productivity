646f6f24c7b526dd630d51560414d7e8345b0338
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-06 12:48:03 -0300

    feat(iservice): add service implementation for ContractProject entity

---
+++ src/Indica.System.Application/Services/ContractProjectService.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using Indica.System.Application.DTO;
+using Indica.System.Application.Interfaces;
+using Indica.System.Domain.Entities;
+using Indica.System.Domain.Interfaces;
+using Indica.System.Shared.Interfaces;
+
+namespace Indica.System.Application.Services
+{
+    public class ContractProjectService : BaseService<ContractProjectDTO, ContractProject>, IContractProjectService
+    {
+        public ContractProjectService(IContractProjectRepository repository, IMapper mapper, IFileParser parser) : base(repository, mapper, parser)
+        {
+        }
+    }
+}
