a600486723467ac851d9f09f27d640869343cbe5
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-06 12:36:11 -0300

    feat(dto): add data transfer object for ContractProject entity

---
+++ src/Indica.System.Application/DTO/ContractProjectDTO.cs
@@ -0,0 +1,9 @@
+namespace Indica.System.Application.DTO
+{
+    public class ContractProjectDTO : EntityBaseDTO
+    {
+        public int IdContract { get; set; }
+        public int IdRegional { get; set; }
+        public int IdProject { get; set; }
+    }
+}
