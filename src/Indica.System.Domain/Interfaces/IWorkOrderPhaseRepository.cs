b55846987e34162884b219b6bea218bbaf858100
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-06 09:13:43 -0300

    feat(irepository): add WorkOrderPhase interface repository

---
+++ src/Indica.System.Domain/Interfaces/IWorkOrderPhaseRepository.cs
@@ -0,0 +1,8 @@
+using Indica.System.Domain.Entities;
+
+namespace Indica.System.Domain.Interfaces
+{
+    public interface IWorkOrderPhaseRepository : IBaseRepository<WorkOrderPhase>
+    {
+    }
+}
