5672afe0a5f15f09c3fe2375f87edf77cd27174c
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-06 09:00:29 -0300

    refactor(entity): replace properties `IdContract` and `IdProcess` to reference Contract by `IdContractProject` to reference ContractProject on Payment entity

--- src/Indica.System.Domain/Entities/Payment.cs
+++ src/Indica.System.Domain/Entities/Payment.cs
@@ -2,11 +2,12 @@ namespace Indica.System.Domain.Entities
 {
     public class Payment : EntityBase
     {
-        public int IdContract { get; set; }
-        public int IdProject { get; set; }
+        public int IdContractProject { get; set; }
         public int IdPaymentMaster { get; set; }
         public bool IsCaminhao { get; set; }
         public bool IsEspecial { get; set; }
         public decimal Valuation { get; set; }
+        public ContractProject ContractProject { get; set; }
+        public PaymentMaster Mestre { get; set; }
     }
 }
