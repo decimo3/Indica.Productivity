d6b776cedf1b120ecbad0cbceb621e7ca8c3d831
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-06 09:00:05 -0300

    refactor(entity): replace properties `IdContract` and `IdProcess` to reference Contract by `IdContractProject` to reference ContractProject on Objective entity

--- src/Indica.System.Domain/Entities/Objective.cs
+++ src/Indica.System.Domain/Entities/Objective.cs
@@ -2,8 +2,7 @@ namespace Indica.System.Domain.Entities
 {
     public class Objective : EntityBase
     {
-        public int IdContract { get; set; }
-        public int IdProcess { get; set; }
+        public int IdContractProject { get; set; }
         public bool IsBasketTruck { get; set; }
         public bool IsHalfPrice { get; set; }
         public decimal MonthlyProfitGoal { get; set; }
@@ -11,5 +10,6 @@ namespace Indica.System.Domain.Entities
         public int TargetOfTeamCountOnWorkday { get; set; }
         public int TargetOfTeamCountOnHoliday { get; set; }
         public int TargetOfExecutionsPerDay { get; set; }
+        public ContractProject ContractProject { get; set; }
     }
 }
