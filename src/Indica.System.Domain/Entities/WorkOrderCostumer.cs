64f768a97888cf3fdb7c3e6fb908261f5bd1ddc2
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-07 13:57:53 -0300

    refactor(entity): add 'virtual' keyword to navigations properties on WorkOrderCostumer

--- src/Indica.System.Domain/Entities/WorkOrderCostumer.cs
+++ src/Indica.System.Domain/Entities/WorkOrderCostumer.cs
@@ -20,7 +20,7 @@ namespace Indica.System.Domain.Entities
         public double CoordinateX { get; set; }
         public double CoordinateY { get; set; }
         public int IdCoordinateAccuracy { get; set; }
-        public WorkOrderPhase Phase { get; set; }
-        public WorkOrderAccuracy Accuracy { get; set; }
+        public virtual WorkOrderPhase Phase { get; set; }
+        public virtual WorkOrderAccuracy Accuracy { get; set; }
     }
 }
