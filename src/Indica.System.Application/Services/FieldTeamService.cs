73ef261ed19641899a11ebcf590a9c9f38b1fb41
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-12 17:38:26 -0300

    refactor(service): changed to a structured nongeneric FieldTeam service

--- src/Indica.System.Application/Services/FieldTeamService.cs
+++ src/Indica.System.Application/Services/FieldTeamService.cs
@@ -4,14 +4,74 @@ using Indica.System.Application.Interfaces;
 using Indica.System.Domain.Entities;
 using Indica.System.Domain.Interfaces;
 using Indica.System.Shared.Interfaces;
+using System.Linq.Expressions;
 
 namespace Indica.System.Application.Services
 {
-    public class FieldTeamService : BaseService<FieldTeamDTO, FieldTeam>, IFieldTeamService
+    public class FieldTeamService : IBaseService<FieldTeamDTO, FieldTeam>, IFieldTeamService
     {
-        public FieldTeamService(IFieldTeamRepository repository, IMapper mapper, IFileParser parser) : base(repository, mapper, parser)
+        private readonly IFileParser _parser;
+        private readonly IActivityRepository _activityRepository;
+        private readonly IFieldTeamRepository _fieldteamRepository;
+        private readonly IFieldTeamCoupleRepository _coupleRepository;
+        private readonly IFieldTeamRegionalRepository _regionalRepository;
+        private readonly IFieldTeamFunctionRepository _functionRepository;
+        public FieldTeamService
+        (
+            IFileParser parser,
+            IActivityRepository activityRepository,
+            IFieldTeamRepository fieldteamRepository,
+            IFieldTeamCoupleRepository coupleRepository,
+            IFieldTeamRegionalRepository regionalRepository,
+            IFieldTeamFunctionRepository functionRepository
+        )
         {
-
+            _parser = parser;
+            _activityRepository = activityRepository;
+            _fieldteamRepository = fieldteamRepository;
+            _coupleRepository = coupleRepository;
+            _regionalRepository = regionalRepository;
+            _functionRepository = functionRepository;
+        }
+        public async Task<bool> AddAsync(FieldTeamDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<int> AddRangeAsync(List<FieldTeamDTO> lista)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<int> AddRangeAsync(Stream arquivo, string filename)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<int> DeleteRangeAsync(List<FieldTeamDTO> lista)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<List<FieldTeamDTO>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<List<FieldTeamDTO>> GetByExpression(Expression<Func<FieldTeamDTO, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<FieldTeamDTO> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> UpdateAsync(FieldTeamDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<int> UpdateRangeAsync(List<FieldTeamDTO> lista)
+        {
+            throw new NotImplementedException();
         }
     }
 }
