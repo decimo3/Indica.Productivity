f55a30c115097fb6dc6b7d8af4b022f88667ad27
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-12 17:41:39 -0300

    feat(irepository): add methods to avoid wasting resources by geting all data

--- src/Indica.System.Domain/IBaseRepository.cs
+++ src/Indica.System.Domain/IBaseRepository.cs
@@ -13,5 +13,8 @@ namespace Indica.System.Domain
         Task<int> AddRangeAsync(List<T> lista);
         Task<int> UpdateRangeAsync(List<T> lista);
         Task<int> DeleteRangeAsync(List<T> lista);
+        Task<T?> FirstOrDefaultByExpressionAsync(Expression<Func<T, bool>> eexpression);
+        Task<T> SingleByExpressionAsync(Expression<Func<T, bool>> eexpression);
+        Task<List<T>> GetPagedAndFilteredByExpressionAsync(int offset, int limit, Expression<Func<T, bool>>? filter = null);
     }
 }
