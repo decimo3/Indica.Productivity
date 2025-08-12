b8bcea12c82307a0f5a7e51253db0d4f2e85a1f3
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-12 09:11:30 -0300

    feat(test): add first test to webapi

---
+++ src/Indica.System.Test/EndpointTest.cs
@@ -0,0 +1,66 @@
+﻿using System.Net;
+using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Indica.System.API;
+using Indica.System.Infra;
+
+namespace Indica.System.Test
+{
+    public class MockWebapi : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Remove o DbContext real
+                var descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(ProductivityContext)
+                );
+                if (descriptor != null)
+                    services.Remove(descriptor);
+                // Adiciona um DbContext em memória
+                services.AddDbContext<ProductivityContext>(options =>
+                {
+                    options.UseInMemoryDatabase("TestDb");
+                });
+            });
+        }
+    }
+    public class EndpointTest : IClassFixture<MockWebapi>
+    {
+        private readonly HttpClient _client;
+        public EndpointTest(MockWebapi factory)
+        {
+            _client = factory.CreateClient();
+        }
+        [Fact]
+        public async Task PostRequest_ProcessSingle_ReturnSucess()
+        {
+            // Arrange
+            var url = "/api/process";
+            var json = "{\"processName\":\"CORE\",\"id\":1}";
+            var payload = new StringContent(json,
+                Encoding.UTF8, "application/json");
+            // Act test
+            var response = await _client.PostAsync(url, payload);
+            // Assert
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+        }
+        [Fact]
+        public async Task PutRequest_ProcessSingle_ReturnFailure()
+        {
+            // Arrange
+            var url = "/api/process";
+            var json = "{\"processName\":\"COREASDASDASDASDASDAS\"}";
+            var payload = new StringContent(json,
+                Encoding.UTF8, "application/json");
+            // Act test
+            var response = await _client.PostAsync(url, payload);
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
