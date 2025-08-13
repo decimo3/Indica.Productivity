79457b42be0d6dbded01f8b6e6f20d6b37bb26c3
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-13 08:25:04 -0300

    fix(irepository): add nulability to return on `IBaseRepository`

--- src/Indica.System.Domain/IBaseRepository.cs
+++ src/Indica.System.Domain/IBaseRepository.cs
@@ -14,7 +14,7 @@ namespace Indica.System.Domain
         Task<int> UpdateRangeAsync(List<T> lista);
         Task<int> DeleteRangeAsync(List<T> lista);
         Task<T?> FirstOrDefaultByExpressionAsync(Expression<Func<T, bool>> eexpression);
-        Task<T> SingleByExpressionAsync(Expression<Func<T, bool>> eexpression);
+        Task<T?> SingleOrDefaultByExpressionAsync(Expression<Func<T, bool>> eexpression);
         Task<List<T>> GetPagedAndFilteredByExpressionAsync(int offset, int limit, Expression<Func<T, bool>>? filter = null);
     }
 }
