09eb15d27e3a22d591e27281ee2254cc2978aef6
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-13 14:45:02 -0300

    fix(test): rewritten to improve readability

--- src/Indica.System.Test/EndpointTest.cs
+++ src/Indica.System.Test/EndpointTest.cs
@@ -16,9 +16,8 @@ namespace Indica.System.Test
             builder.ConfigureServices(services =>
             {
                 // Remove o DbContext real
-                var descriptor = services.SingleOrDefault(
-                d => d.ServiceType == typeof(ProductivityContext)
-                );
+                var descriptor = services.SingleOrDefault(d =>
+                    d.ServiceType == typeof(ProductivityContext));
                 if (descriptor != null)
                     services.Remove(descriptor);
                 // Adiciona um DbContext em memória
