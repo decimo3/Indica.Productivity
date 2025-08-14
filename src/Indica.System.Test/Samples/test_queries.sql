51668a5ba44511fafb2334ae4654faaba393e36c
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-14 15:42:20 -0300

    chore(test): add complementary sql to insert mock data on test database

---
+++ src/Indica.System.Test/Samples/test_queries.sql
@@ -0,0 +1,4 @@
+INSERT INTO funcionarios (id_funcionario, matricula_indica, matricula_cliente, nome_funcionario, data_admissao, id_funcionario_funcao, id_funcionario_situacao) VALUES
+(1, 1111111, 1111111, 'Fulano De Tal', '2025-01-01', 1, 1),
+(2, 2222222, 2222222, 'Ciclano De Tal', '2025-01-01', 1, 1),
+(3, 3333333, 3333333, 'Beltrano De Tal', '2025-01-01', 1, 1);
