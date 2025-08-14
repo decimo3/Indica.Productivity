fe4248c9b7f625c130a3c5b5ac781ff5600dd3a2
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-14 11:08:52 -0300

    refactor(iservice): rename method to fit naming pattern on `IBaseRepository`

--- src/Indica.System.Domain/IBaseRepository.cs
+++ src/Indica.System.Domain/IBaseRepository.cs
@@ -13,8 +13,8 @@ namespace Indica.System.Domain
         Task<int> AddRangeAsync(List<T> lista);
         Task<int> UpdateRangeAsync(List<T> lista);
         Task<int> DeleteRangeAsync(List<T> lista);
-        Task<T?> FirstOrDefaultByExpressionAsync(Expression<Func<T, bool>> eexpression);
-        Task<T?> SingleOrDefaultByExpressionAsync(Expression<Func<T, bool>> eexpression);
+        Task<T?> GetFirstOrDefaultByExpressionAsync(Expression<Func<T, bool>> eexpression);
+        Task<T?> GetSingleOrDefaultByExpressionAsync(Expression<Func<T, bool>> eexpression);
         Task<List<T>> GetPagedAndFilteredByExpressionAsync(int offset, int limit, Expression<Func<T, bool>>? filter = null);
     }
 }
