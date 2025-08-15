d9738dd544c017c1e918d672a57a2d977fa25e27
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-15 15:37:46 -0300

    refactor(service): implemented `GetRegionalAsync` method on `FieldTeamService.AddAsync`.

--- src/Indica.System.Application/Services/FieldTeamService.cs
+++ src/Indica.System.Application/Services/FieldTeamService.cs
@@ -53,9 +53,7 @@ namespace Indica.System.Application.Services
             var erros = entity.Validate();
             if (erros.Count != 0)
                 throw new InvalidOperationException("Há erros de validação na informação enviada!");
-            var regional = await _regionalRepository.GetSingleOrDefaultByExpressionAsync(
-                r => r.RegionName == entity.WorkArea) ??
-                    throw new InvalidOperationException("A regional informada não foi encontrada!");
+            var regional = await GetRegionalAsync(entity.WorkArea);
             var activity = await _activityRepository.GetSingleOrDefaultByExpressionAsync(
                 a => a.ActivityName == entity.ActivityName) ??
                     throw new InvalidOperationException("A atividade informada não foi encontrada!");
