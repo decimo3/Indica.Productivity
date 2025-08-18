12526a99c79a8ecccc8624556b9ee4e729c10836
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-18 10:08:16 -0300

    refactor(entity): add keyword `virtual` to navigation property to allow Lazy Loading on `FinishingPayment`.

--- src/Indica.System.Domain/Entities/FinishingPayment.cs
+++ src/Indica.System.Domain/Entities/FinishingPayment.cs
@@ -4,6 +4,6 @@
     {
         public string IdFinishing { get; set; }
         public int IdPaymentMaster { get; set; }
-        public FinishingPayment Mestre { get; set; }
+        public virtual FinishingPayment Mestre { get; set; }
     }
 }
