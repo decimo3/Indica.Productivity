86d9681672b00eb7fc1c07a49ddbbcf014a25d8a
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-18 10:06:45 -0300

    refactor(entity): add keyword `virtual` to navigation property to allow Lazy Loading on `ContractProject`.

--- src/Indica.System.Domain/Entities/ContractProject.cs
+++ src/Indica.System.Domain/Entities/ContractProject.cs
@@ -5,8 +5,8 @@
         public int IdContract { get; set; }
         public int IdRegional { get; set; }
         public int IdProject { get; set; }
-        public Contract Contract { get; set; }
-        public FieldTeamRegional Regional { get; set; }
-        public Project Project { get; set; }
+        public virtual Contract Contract { get; set; }
+        public virtual FieldTeamRegional Regional { get; set; }
+        public virtual Project Project { get; set; }
     }
 }
