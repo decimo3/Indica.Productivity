29ecf22172c316624700b9257bbc47e4608ece3e
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-19 14:03:57 -0300

    feat(automapper): replace `FinishingPaymentAutoMapper` to `FinishingAutoMapper` and configure automapper properly

--- src/Indica.System.Application/Mappers/FinishingAutoMapper.cs
+++ src/Indica.System.Application/Mappers/FinishingAutoMapper.cs
@@ -1,14 +1,17 @@
-﻿using AutoMapper;
+using AutoMapper;
 using Indica.System.Application.DTO;
 using Indica.System.Domain.Entities;
 
 namespace Indica.System.Application.Mappers
 {
-    public class FinishingPaymentAutoMapper : Profile
+    public class FinishingAutoMapper : Profile
     {
-        public FinishingPaymentAutoMapper()
+        public FinishingAutoMapper()
         {
-            CreateMap<FinishingPaymentDTO, FinishingPayment>().ReverseMap();
+            CreateMap<Finishing, FinishingDTO>()
+                .ForMember(dest => dest.GroupingOfMeasures, opt => opt.MapFrom(src => src.GroupingOfMeasures))
+                .ForMember(dest => dest.FinishingDetail, opt => opt.MapFrom(src => src.Detail.Detail))
+                .ForMember(dest => dest.PaymentMasters, opt => opt.MapFrom(src => string.Join('/', src.Payments.Select(p => p.Mestre.Master).ToList())));
         }
     }
 }
