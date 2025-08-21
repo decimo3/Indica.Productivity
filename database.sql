38114e1390cc3f4b75cd3df3ef2cdb77ef65f314
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-21 17:46:27 -0300

    chore(database): change data seed to exact text case-sensitive that came from report on table `dano_projeto`.

--- database.sql
+++ database.sql
@@ -230,7 +230,7 @@ CREATE TABLE IF NOT EXISTS dano_projeto (
 INSERT INTO dano_projeto (id_dano_projeto, nome_dano_projeto, texto_breve_para_dano, id_projeto) VALUES
 (1, '0001', 'Início de turno', NULL),
 (2, '0002', 'Intervalo para almoço', NULL),
-(3, '0003', 'Indisponibilidade', NULL),
+(3, '0003', 'INDISPONIBILIDADE', NULL),
 (4, '0004', 'Retorno para base', NULL);
 
 
