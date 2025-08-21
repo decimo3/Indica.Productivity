a612107c07efbb389d64d4f34ecb6a84d561053f
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-21 13:38:13 -0300

    feat(irepository): add `GetAllIdsByActivityAsync` specific method to `WorkOrderBase` children repositories to get a list of IDs if it is on database

--- src/Indica.System.Domain/Interfaces/IWorkOrderServiceRepository.cs
+++ src/Indica.System.Domain/Interfaces/IWorkOrderServiceRepository.cs
@@ -2,5 +2,8 @@ using Indica.System.Domain.Entities;
 
 namespace Indica.System.Domain.Interfaces
 {
-    public interface IWorkOrderServiceRepository : IBaseRepository<WorkOrderService> { }
+    public interface IWorkOrderServiceRepository : IBaseRepository<WorkOrderService>
+    {
+        Task<List<long>> GetAllIdsByActivityAsync(List<long> ids);
+    }
 }
--- src/Indica.System.Domain/Interfaces/IWorkOrderShiftInfoRepository.cs
+++ src/Indica.System.Domain/Interfaces/IWorkOrderShiftInfoRepository.cs
@@ -2,5 +2,8 @@ using Indica.System.Domain.Entities;
 
 namespace Indica.System.Domain.Interfaces
 {
-    public interface IWorkOrderShiftInfoRepository : IBaseRepository<WorkOrderShiftInfo> { }
+    public interface IWorkOrderShiftInfoRepository : IBaseRepository<WorkOrderShiftInfo>
+    {
+        Task<List<long>> GetAllIdsByActivityAsync(List<long> ids);
+    }
 }
