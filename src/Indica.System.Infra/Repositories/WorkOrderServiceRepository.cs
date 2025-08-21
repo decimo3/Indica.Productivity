1d98850d7afdb023420e31d12b0506ec7b986b45
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-21 13:40:35 -0300

    feat(repository): Add implementation of method `GetAllIdsByActivityAsync` to get a list of IDs of `WorkOrderBase` children entities if it is on database

--- src/Indica.System.Infra/Repositories/WorkOrderServiceRepository.cs
+++ src/Indica.System.Infra/Repositories/WorkOrderServiceRepository.cs
@@ -1,10 +1,22 @@
 using Indica.System.Domain.Entities;
 using Indica.System.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Indica.System.Infra.Repositories
 {
     public class WorkOrderServiceRepository : BaseRepository<WorkOrderService>, IWorkOrderServiceRepository
     {
-        public WorkOrderServiceRepository(ProductivityContext context) : base(context) {}
+        private readonly DbContext context;
+        public WorkOrderServiceRepository(ProductivityContext context) : base(context)
+        {
+            this.context = context;
+        }
+        public async Task<List<long>> GetAllIdsByActivityAsync(List<long> ids)
+        {
+            return await context.Set<WorkOrderService>()
+                .Where(e => ids.Contains(e.IdActivity))
+                .Select(e => e.IdActivity)
+                .ToListAsync();
+        }
     }
 }
--- src/Indica.System.Infra/Repositories/WorkOrderShiftInfoRepository.cs
+++ src/Indica.System.Infra/Repositories/WorkOrderShiftInfoRepository.cs
@@ -1,10 +1,22 @@
 using Indica.System.Domain.Entities;
 using Indica.System.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Indica.System.Infra.Repositories
 {
     public class WorkOrderShiftInfoRepository : BaseRepository<WorkOrderShiftInfo>, IWorkOrderShiftInfoRepository
     {
-        public WorkOrderShiftInfoRepository(ProductivityContext context) : base(context) {}
+        private readonly DbContext context;
+        public WorkOrderShiftInfoRepository(ProductivityContext context) : base(context)
+        {
+            this.context = context;
+        }
+        public async Task<List<long>> GetAllIdsByActivityAsync(List<long> ids)
+        {
+            return await context.Set<WorkOrderShiftInfo>()
+                .Where(e => ids.Contains(e.IdActivity))
+                .Select(e => e.IdActivity)
+                .ToListAsync();
+        }
     }
 }
