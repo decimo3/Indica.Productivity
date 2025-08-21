21f67db08b183150989185abe0bb91ad806e586a
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-21 09:04:02 -0300

    refactor(entity): add property `UnavailableReasonOrIntervalDescription` from `WorkOrderInterval` to `WorkOrderShiftInfo`. Add nullability to all properties due to two tables merged into one.

--- src/Indica.System.Domain/Entities/WorkOrderShiftInfo.cs
+++ src/Indica.System.Domain/Entities/WorkOrderShiftInfo.cs
@@ -2,10 +2,11 @@ namespace Indica.System.Domain.Entities
 {
     public class WorkOrderShiftInfo : WorkOrderBase
     {
-        public DateTime ShiftStartDate { get; set; }
-        public string VehicleLabel { get; set; }
-        public int IdLeaderRegistration { get; set; }
-        public int IdAuxiliaryRegistration { get; set; }
-        public int IdTechnicalRegistration { get; set; }
+        public DateTime? ShiftStartDate { get; set; }
+        public string? VehicleLabel { get; set; }
+        public int? IdLeaderRegistration { get; set; }
+        public int? IdAuxiliaryRegistration { get; set; }
+        public int? IdTechnicalRegistration { get; set; }
+        public string? UnavailableReasonOrIntervalDescription { get; set; }
     }
 }
