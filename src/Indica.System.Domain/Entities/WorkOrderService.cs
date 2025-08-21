cb23b5730f41a48c58f42543911a3cc908651295
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-21 13:43:59 -0300

    fix(entity): add nullability to related entity `Finishing` on `WorkOrderService`

--- src/Indica.System.Domain/Entities/WorkOrderService.cs
+++ src/Indica.System.Domain/Entities/WorkOrderService.cs
@@ -26,7 +26,7 @@ namespace Indica.System.Domain.Entities
         public int CHI { get; set; }
         public int InterruptedTime { get; set; }
         public int FinancialCompensationAmount { get; set; }
-        public int IdFinishing { get; set; }
+        public int? IdFinishing { get; set; }
         public virtual Finishing Finishing { get; set; }
         public virtual WorkOrderCostumer WorkOrderCostumer { get; set; }
     }
