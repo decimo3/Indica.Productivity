ea93c8b1643ab3a19adda65302fd10854ef0d282
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-22 16:42:35 -0300

    feat(irepository): add `GetAllIdsByInstallationAsync` specific method to `IWorkOrderCostumerRepository` to get a list of IDs and installations if it is on database

--- src/Indica.System.Domain/Interfaces/IWorkOrderCostumerRepository.cs
+++ src/Indica.System.Domain/Interfaces/IWorkOrderCostumerRepository.cs
@@ -2,5 +2,8 @@ using Indica.System.Domain.Entities;
 
 namespace Indica.System.Domain.Interfaces
 {
-    public interface IWorkOrderCostumerRepository : IBaseRepository<WorkOrderCostumer> { }
+    public interface IWorkOrderCostumerRepository : IBaseRepository<WorkOrderCostumer>
+    {
+        Task<List<(long InstallationNumber, long Id)>> GetAllIdsByInstallationAsync(List<long> ids);
+    }
 }
