388dcec87cdc912278c93d296624800798f485b8
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-22 16:43:56 -0300

    feat(repository): Add implementation of method `GetAllIdsByInstallationAsync` to get a list of IDs and iinstallations of `WorkOrderCostumer` if it is on database

--- src/Indica.System.Infra/Repositories/WorkOrderCostumerRepository.cs
+++ src/Indica.System.Infra/Repositories/WorkOrderCostumerRepository.cs
@@ -1,10 +1,22 @@
 using Indica.System.Domain.Entities;
 using Indica.System.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Indica.System.Infra.Repositories
 {
     public class WorkOrderCostumerRepository : BaseRepository<WorkOrderCostumer>, IWorkOrderCostumerRepository
     {
-        public WorkOrderCostumerRepository(ProductivityContext context) : base(context) {}
+        private readonly DbContext context;
+        public WorkOrderCostumerRepository(ProductivityContext context) : base(context)
+        {
+            this.context = context;
+        }
+        public async Task<List<(long InstallationNumber, long Id)>> GetAllIdsByInstallationAsync(List<long> ids)
+        {
+            return await context.Set<WorkOrderCostumer>()
+                .Where(e => ids.Contains(e.InstallationNumber))
+                .Select(e => new ValueTuple<long, long>(e.InstallationNumber, e.Id))
+                .ToListAsync();
+        }
     }
 }
