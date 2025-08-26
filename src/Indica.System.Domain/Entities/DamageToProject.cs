862caf22a1f3919c91b320cf5d38c76f60a6a6a8
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-08-26 10:24:27 -0300

    feat(entity): add navigation property `Project` on `DamageToProject`

--- src/Indica.System.Domain/Entities/DamageToProject.cs
+++ src/Indica.System.Domain/Entities/DamageToProject.cs
@@ -5,5 +5,6 @@ namespace Indica.System.Domain.Entities
         public string Damage { get; set; }
         public string Description { get; set; }
         public int IdProject { get; set; }
+        public virtual Project Project { get; set; }
     }
 }
