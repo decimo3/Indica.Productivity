400c4663057393f5e8090fc381e7095189eab0d2
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-09-02 12:05:51 -0300

    fix(shared): remove allow multiples because value of second override the first value

--- src/Indica.System.Shared/AliasAttribute.cs
+++ src/Indica.System.Shared/AliasAttribute.cs
@@ -1,6 +1,6 @@
 namespace Indica.System.Shared
 {
-    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
     public class AliasAttribute : Attribute
     {
         public string Name { get; }
