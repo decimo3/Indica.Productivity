dfd005455d1582d13534a6021bb38d5d36728a0b
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-09-03 17:29:33 -0300

    chore(database): correct columns names and relationship of table `finalizacoes_pagamento`

--- database.sql
+++ database.sql
@@ -213,8 +213,8 @@ CREATE TABLE IF NOT EXISTS finalizacoes (
 CREATE TABLE IF NOT EXISTS finalizacoes_pagamento (
     id_finalizacao_pagamento INTEGER PRIMARY KEY,
     id_finalizacao INTEGER REFERENCES finalizacoes(id_finalizacao),
-    id_pagamento INTEGER REFERENCES pagamentos(id_pagamento),
-    UNIQUE (id_finalizacao, id_pagamento)
+    id_mestre INTEGER REFERENCES mestres(id_mestre),
+    UNIQUE (id_finalizacao, id_mestre)
 );
 
 CREATE TABLE IF NOT EXISTS dano_projeto (
