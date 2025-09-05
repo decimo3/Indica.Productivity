ee8cfc544e31d3ee77f70320cb03ea1c8c6e82eb
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-09-05 17:22:16 -0300

    feat(mapper): add mapper to configure binding to `Derivation` entity from database

---
+++ src/Indica.System.Infra/EntityMappers/DerivationMapper.cs
@@ -0,0 +1,35 @@
+using Indica.System.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Indica.System.Infra.EntityMappers
+{
+    public class DerivationMapper : IEntityTypeConfiguration<Derivation>
+    {
+        public void Configure(EntityTypeBuilder<Derivation> builder)
+        {
+            builder.ToTable("derivacoes");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .HasColumnName("id_derivacao")
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+            builder.Property(x => x.DerivationName)
+                .HasColumnName("nome_derivacao")
+                .HasMaxLength(16)
+                .IsRequired();
+            builder.HasIndex(x => new { x.DerivationName }).IsUnique();
+            builder.HasData([
+                new Derivation() { Id = 1,  DerivationName = "CONVENCIONAL" },
+                new Derivation() { Id = 2,  DerivationName = "CAMINHÃO CESTO" },
+                new Derivation() { Id = 3,  DerivationName = "INICIATIVA" },
+                new Derivation() { Id = 4,  DerivationName = "MANUTENÇÃO BT" },
+                new Derivation() { Id = 5, DerivationName = "EXTERNALIZAÇÃO" },
+                new Derivation() { Id = 6, DerivationName = "MODERNIZAÇÃO" },
+                new Derivation() { Id = 7, DerivationName = "NORMALIZAÇÃO" },
+                new Derivation() { Id = 8,  DerivationName = "VISTORIADOR" },
+                new Derivation() { Id = 9,  DerivationName = "EMERGÊNCIA" },
+            ]);
+        }
+    }
+}
