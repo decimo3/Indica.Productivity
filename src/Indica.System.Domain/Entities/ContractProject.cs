1bb3e588dcd5136e1a89843bd58209824f5eccd1
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-09-05 17:29:52 -0300

    feat(entity): add `IdDerivation` and navigation properties `Derivation` to bind `Project` to `Derivation` on `ContractProject` entity

--- src/Indica.System.Domain/Entities/ContractProject.cs
+++ src/Indica.System.Domain/Entities/ContractProject.cs
@@ -5,8 +5,10 @@
         public int IdContract { get; set; }
         public int IdRegional { get; set; }
         public int IdProject { get; set; }
+        public int IdDerivation { get; set; }
         public virtual Contract Contract { get; set; }
         public virtual FieldTeamRegional Regional { get; set; }
         public virtual Project Project { get; set; }
+        public virtual Derivation Derivation { get; set; }
     }
 }
