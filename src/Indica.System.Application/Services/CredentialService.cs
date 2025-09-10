ae6c0f01a86fec4dae9ff9b76b32fe0ec03fe388
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-09-10 10:44:43 -0300

    feat(service): add concrete service to handle with `Credential` endpoint

---
+++ src/Indica.System.Application/Services/CredentialService.cs
@@ -0,0 +1,89 @@
+using Indica.System.Application.Interfaces;
+using Indica.System.Domain.Interfaces;
+using Indica.System.Domain.Entities;
+using System.Security.Cryptography;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using Microsoft.Extensions.Configuration;
+
+namespace Indica.System.Application.Services
+{
+    public class CredentialService : ICredentialService
+    {
+        private readonly IConfiguration configuration;
+        private readonly ICredentialRepository credentialRepository;
+        public CredentialService
+        (
+            IConfiguration configuration,
+            ICredentialRepository credentialRepository
+        )
+        {
+            this.configuration = configuration;
+            this.credentialRepository = credentialRepository;
+        }
+
+        public async Task<string> GetPassHash(string password)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(password);
+            var bytes = SHA256.HashData(Encoding.UTF8.GetBytes(password));
+            var hash = BitConverter.ToString(bytes).Replace("-", "").ToLowerInvariant();
+            return hash;
+        }
+
+        public async Task<Employer?> ValidateUser(int registry, string password)
+        {
+            var passhash = await GetPassHash(password);
+            return (await credentialRepository.GetSingleOrDefaultByExpressionAsync(
+                c => c.IdEmployer == registry && c.PassHash == passhash))?.Employer;
+        }
+
+        public async Task<string> GenerateToken(Employer employer)
+        {
+            var secret = configuration["SecretKey"] ??
+                throw new InvalidOperationException("A configuração `SecretKey` não foi definida!");
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, employer.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.UniqueName, employer.FullName ?? string.Empty)
+                // TODO - Add more claims as needed
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: "Indica.System",
+                audience: "Indica.System.Users",
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(8),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public async Task<Employer?> ValidateToken(string token)
+        {
+            var secret = configuration["SecretKey"] ??
+                throw new InvalidOperationException("A configuração `SecretKey` não foi definida!");
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(secret);
+            var issuerSiginingKey = new SymmetricSecurityKey(key);
+            var tokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = issuerSiginingKey,
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ClockSkew = TimeSpan.Zero
+            };
+            tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken validatedToken);
+            var jwtToken = (JwtSecurityToken)validatedToken;
+            var userId = int.Parse(jwtToken.Claims.First(x => x.Type == JwtRegisteredClaimNames.Sub).Value);
+            return (await credentialRepository.GetSingleOrDefaultByExpressionAsync(
+                c => c.IdEmployer == userId))?.Employer;
+        }
+    }
+}
