7556f6490ea1c340b16a200449828ac09d255107
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-01-07 15:10:51 -0300

    feat(service): add function `GetContractProject` to get entity from DTOs

--- src/Indica.Productivity.Application/Services/ContractProjectService.cs
+++ src/Indica.Productivity.Application/Services/ContractProjectService.cs
@@ -9,8 +9,45 @@ namespace Indica.Productivity.Application.Services
 {
     public class ContractProjectService : BaseService<ContractProjectDTO, ContractProject>, IContractProjectService
     {
-        public ContractProjectService(IContractProjectRepository repository, IMapper mapper, IFileParser parser) : base(repository, mapper, parser)
+        private readonly IContractRepository contractRepository;
+        private readonly IProjectRepository projectRepository;
+        private readonly IDerivationRepository derivationRepository;
+        private readonly IFieldTeamRegionalRepository regionalRepository;
+        private readonly IContractProjectRepository contractProjectRepository;
+        public ContractProjectService
+        (
+            IMapper mapper,
+            IFileParser parser,
+            IContractRepository contractRepository,
+            IProjectRepository projectRepository,
+            IDerivationRepository derivationRepository,
+            IFieldTeamRegionalRepository regionalRepository,
+            IContractProjectRepository contractProjectRepository
+        ) : base(contractProjectRepository, mapper, parser)
         {
+            this.contractRepository = contractRepository;
+            this.projectRepository = projectRepository;
+            this.derivationRepository = derivationRepository;
+            this.regionalRepository = regionalRepository;
+            this.contractProjectRepository = contractProjectRepository;
+        }
+        private async Task<List<ContractProject>> GetContractProject(List<ContractProjectDTO> entities)
+        {
+            var contracts = await contractRepository.GetAllAsync();
+            var projects = await projectRepository.GetAllAsync();
+            var derivations = await derivationRepository.GetAllAsync();
+            var regions = await regionalRepository.GetAllAsync();
+            var tasks = entities.Select(async e =>
+            {
+                return new ContractProject
+                {
+                    IdContract = contracts.Single(c => c.ContractNumber == e.Contract && c.AdditiveNumber == e.Additive).Id,
+                    IdProject = projects.Single(p => p.ProjectName == e.Project).Id,
+                    IdDerivation = derivations.Single(d => d.DerivationName == e.Derivation).Id,
+                    IdRegional = regions.Single(r => r.RegionName == e.Regional).Id
+                };
+            });
+            return (await Task.WhenAll(tasks)).ToList();
         }
     }
 }
