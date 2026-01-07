d483b5513f12097face802fee2a2759c94429673
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-01-07 17:03:53 -0300

    fix(automappers): add instructions to convert `ContractProject` to `ContractProjectDTO`

--- src/Indica.Productivity.Application/Mappers/ContractProjectAutoMapper.cs
+++ src/Indica.Productivity.Application/Mappers/ContractProjectAutoMapper.cs
@@ -8,7 +8,12 @@ namespace Indica.Productivity.Application.Mappers
     {
         public ContractProjectAutoMapper()
         {
-            CreateMap<ContractProject, ContractProjectDTO>().ReverseMap();
+            CreateMap<ContractProject, ContractProjectDTO>()
+                .ForMember(dest => dest.Contract, opt => opt.MapFrom(src => src.Contract.ContractNumber))
+                .ForMember(dest => dest.Additive, opt => opt.MapFrom(src => src.Contract.AdditiveNumber))
+                .ForMember(dest => dest.Project, opt => opt.MapFrom(src => src.Project.ProjectName))
+                .ForMember(dest => dest.Derivation, opt => opt.MapFrom(src => src.Derivation.DerivationName))
+                .ForMember(dest => dest.Regional, opt => opt.MapFrom(src => src.Regional.RegionName));
         }
     }
 }
