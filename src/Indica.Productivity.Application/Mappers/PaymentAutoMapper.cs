af2c2183d0711fe57c126499e027d68acf7dd7cd
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-01-15 15:59:54 -0300

    refactor(autoMapper): updated AutoMapper of `PaymentAutoMapper`

--- src/Indica.Productivity.Application/Mappers/PaymentAutoMapper.cs
+++ src/Indica.Productivity.Application/Mappers/PaymentAutoMapper.cs
@@ -8,7 +8,13 @@ namespace Indica.Productivity.Application.Mappers
     {
         public PaymentAutoMapper()
         {
-            CreateMap<Payment, PaymentDTO>().ReverseMap();
+            CreateMap<Payment, PaymentDTO>()
+                .ForMember(dest => dest.ContractNumber, opt => opt.MapFrom(src => src.ContractProject.Contract.ContractNumber))
+                .ForMember(dest => dest.AdditiveNumber, opt => opt.MapFrom(src => src.ContractProject.Contract.AdditiveNumber))
+                .ForMember(dest => dest.ProjectName, opt => opt.MapFrom(src => src.ContractProject.Project.ProjectName))
+                .ForMember(dest => dest.Derivation, opt => opt.MapFrom(src => src.ContractProject.Derivation.DerivationName))
+                .ForMember(dest => dest.Master, opt => opt.MapFrom(src => src.Mestre))
+                .ForMember(dest => dest.Valuation, opt => opt.MapFrom(src => src.Valuation));
         }
     }
 }
