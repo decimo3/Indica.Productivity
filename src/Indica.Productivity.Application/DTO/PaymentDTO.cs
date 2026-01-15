94003d79a2b7e1e0648c713bce1b26de338eb9ea
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-01-15 15:58:26 -0300

    refactor(dto): add properties on `PaymentDTO` to easier data insertion

--- src/Indica.Productivity.Application/DTO/PaymentDTO.cs
+++ src/Indica.Productivity.Application/DTO/PaymentDTO.cs
@@ -1,9 +1,22 @@
-﻿namespace Indica.Productivity.Application.DTO
+﻿using Indica.Productivity.Shared;
+
+namespace Indica.Productivity.Application.DTO
 {
     public class PaymentDTO : EntityBaseDTO
     {
-        public int IdContractProject { get; set; }
-        public int IdPaymentMaster { get; set; }
+        [Alias("Contrato")]
+        public long ContractNumber { get; set; }
+        [Alias("Aditivo")]
+        public int AdditiveNumber { get; set; }
+        [Alias("Regional")]
+        public string Regional { get; set; }
+        [Alias("Projeto")]
+        public string ProjectName { get; set; }
+        [Alias("Derivação")]
+        public string Derivation { get; set; }
+        [Alias("Mestre")]
+        public int Master { get; set; }
+        [Alias("Valor")]
         public decimal Valuation { get; set; }
     }
 }
