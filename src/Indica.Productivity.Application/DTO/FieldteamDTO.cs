44e2356d3a4d1e1f30abbd0c78bfb56f8fecd9f8
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-06-22 19:41:12 -0300

    feat: initialize Date property and add Abreviate method for FieldTeamDTO

--- src/Indica.Productivity.Application/DTO/FieldteamDTO.cs
+++ src/Indica.Productivity.Application/DTO/FieldteamDTO.cs
@@ -4,7 +4,7 @@ namespace Indica.Productivity.Application.DTO
     public class FieldTeamDTO : EntityBaseDTO
     {
         [Alias("DATA")]
-        public DateOnly Date { get; set; }
+        public DateOnly Date { get; set; } = DateOnly.FromDateTime(DateTime.Now);
         [Alias("ADESIVO LIGHT")]
         public int Order { get; set; }
         [Alias("PLACA")]
@@ -29,5 +29,25 @@ namespace Indica.Productivity.Application.DTO
         public string SupervisorName { get; set; }
         [Alias("ÁREA DE ATUAÇÃO")]
         public string WorkArea { get; set; }
+        public bool IsConsidered { get; set; } = true;
+        public string Abreviate
+        {
+            get
+            {
+                return Resource
+                        .Trim()
+                        .Replace('–', '-')
+                        .Replace('\u00A0', '\u0020')
+                        .Replace("  ", " ")
+                        .Replace(" - Corte", "C")
+                        .Replace(" - Religa", "R")
+                        .Replace(" - Vistoriador ", "V")
+                        .Replace(" - Indica", "")
+                        .Replace(" - Equipe ", "")
+                        .Replace(" - ", "")
+                        .Split(' ', 2)
+                        .First();
+            }
+        }
     }
 }
