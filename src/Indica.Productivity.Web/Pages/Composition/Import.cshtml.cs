22adacff2e1cea09d006f3e98e503ff4c4f06b89
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-06-22 19:36:32 -0300

    feat: add Import page with file upload functionality and localization support

---
+++ src/Indica.Productivity.Web/Pages/Composition/Import.cshtml.cs
@@ -0,0 +1,43 @@
+using Indica.Productivity.Application.Interfaces;
+using Indica.Productivity.Shared.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Localization;
+
+namespace Indica.Productivity.Web.Pages.Composition
+{
+    public class ImportModel : PageModel
+    {
+        [BindProperty]
+        public Exception? Error { get; set; }
+        [BindProperty]
+        public IFormFile FileSent { get; set; }
+        private readonly IStringLocalizer<SharedResources> _sharedLocalizer;
+        private readonly IEmployerService _service;
+
+        public ImportModel
+        (
+            IStringLocalizer<SharedResources> sharedLocalizer,
+            IEmployerService service
+        )
+        {
+            _service = service;
+            _sharedLocalizer = sharedLocalizer;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            try
+            {
+                await _service.AddRangeAsync(FileSent.OpenReadStream(), FileSent.FileName);
+            }
+            catch (Exception ex)
+            {
+                Error = ex;
+                return Page();
+            }
+            return RedirectToPage("./Index");
+        }
+    }
+}
