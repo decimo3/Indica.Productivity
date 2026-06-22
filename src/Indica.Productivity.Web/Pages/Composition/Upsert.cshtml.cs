d36398bd101025ef7e299f5b0ad97c4fef07a772
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-06-22 19:34:12 -0300

    feat: add Upsert page for composition with form and data binding

---
+++ src/Indica.Productivity.Web/Pages/Composition/Upsert.cshtml.cs
@@ -0,0 +1,62 @@
+using Indica.Productivity.Application.DTO;
+using Indica.Productivity.Application.Interfaces;
+using Indica.Productivity.Domain.Entities;
+using Indica.Productivity.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Indica.Productivity.Web.Pages.Composition;
+
+public class UpsertModel : PageModel
+{
+    private readonly IFieldTeamService _fieldTeamService;
+    private readonly IActivityRepository _fieldTeamActivityRepository;
+    private readonly IFieldTeamRegionalRepository _fieldTeamRegionalRepository;
+
+
+    public UpsertModel
+    (
+        IFieldTeamService fieldTeamService,
+        IActivityRepository fieldTeamActivityRepository,
+        IFieldTeamRegionalRepository fieldTeamRegionalRepository
+    )
+    {
+        _fieldTeamService = fieldTeamService;
+        _fieldTeamActivityRepository = fieldTeamActivityRepository;
+        _fieldTeamRegionalRepository = fieldTeamRegionalRepository;
+    }
+
+    public bool IsEdit { get; private set; } = false;
+    [BindProperty]
+    public FieldTeamDTO Composition { get; set; } = new();
+    public SelectList FieldTeamActivity { get; set; } = null;
+    public SelectList FieldTeamRegional { get; set; } = null;
+
+    private async Task LoadEnumeratorsAsync()
+    {
+        FieldTeamActivity = new SelectList(
+            await _fieldTeamActivityRepository.GetAllAsync(),
+            nameof(Indica.Productivity.Domain.Entities.Activity.ActivityName),
+            nameof(Indica.Productivity.Domain.Entities.Activity.ActivityName),
+            Composition.ActivityName
+            );
+        FieldTeamRegional = new SelectList(
+            await _fieldTeamRegionalRepository.GetAllAsync(),
+            nameof(Indica.Productivity.Domain.Entities.FieldTeamRegional.RegionName),
+            nameof(Indica.Productivity.Domain.Entities.FieldTeamRegional.RegionName),
+            Composition.WorkArea
+            );
+    }
+
+    public async Task<IActionResult> OnGetAsync(int id = 0)
+    {
+        if (id != 0)
+        {
+            try { Composition = await _fieldTeamService.GetByIdAsync(id); }
+            catch { return NotFound(); }
+        }
+        await LoadEnumeratorsAsync();
+        return Page();
+    }
+}
