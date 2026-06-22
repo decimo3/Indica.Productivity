ee3761c2562e800a557d44fbd39eb9fd3eb92cca
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-06-22 19:16:47 -0300

    refactor: restructure EmployerService constructor and implement DeleteAsync method

--- src/Indica.Productivity.Application/Services/EmployerService.cs
+++ src/Indica.Productivity.Application/Services/EmployerService.cs
@@ -1,4 +1,4 @@
-﻿using AutoMapper;
+using AutoMapper;
 using Indica.Productivity.Application.DTO;
 using Indica.Productivity.Application.Interfaces;
 using Indica.Productivity.Domain.Entities;
@@ -9,6 +9,27 @@ namespace Indica.Productivity.Application.Services
 {
     public class EmployerService : BaseService<EmployerDTO, Employer>, IEmployerService
     {
-        public EmployerService(IEmployerRepository repository, IMapper mapper, IFileParser parser) : base(repository, mapper, parser) { }
+        private static readonly int TERMINATED_CODE = 5;
+        private readonly IEmployerRepository _repository;
+        private readonly IEmployerSituationRepository _situationRepository;
+        public EmployerService
+        (
+            IMapper mapper,
+            IFileParser parser,
+            IEmployerRepository repository,
+            IEmployerSituationRepository situationRepository
+        ) : base(repository, mapper, parser)
+        {
+            _repository = repository;
+            _situationRepository = situationRepository;
+        }
+        public override async Task<bool> DeleteAsync(int id)
+        {
+            var employer = await _repository.GetByIdAsync(id) ?? throw new ArgumentException();
+            employer.Demission = DateOnly.FromDateTime(DateTime.Now);
+            employer.Situation = await _situationRepository.GetByIdAsync(TERMINATED_CODE);
+            await _repository.UpdateAsync(employer);
+            return true;
+        }
     }
 }
