36673a6f1a4734292dbb9bfafe5c1ff7053c7df5
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-06-23 08:23:02 -0300

    feat: add report page with file upload and display functionality

---
+++ src/Indica.Productivity.Web/Pages/Report/Index.cshtml.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Indica.Productivity.Application.Interfaces;
+using Indica.Productivity.Application.DTO;
+using Microsoft.Extensions.Localization;
+
+namespace Indica.Productivity.Web.Pages.Report;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    private readonly IWorkOrderService _service;
+    private readonly IStringLocalizer<SharedResources> _sharedLocalizer;
+
+    public IndexModel
+    (
+        ILogger<IndexModel> logger,
+        IStringLocalizer<SharedResources> sharedLocalizer,
+        IWorkOrderService service
+    )
+    {
+        _logger = logger;
+        _service = service;
+        _sharedLocalizer = sharedLocalizer;
+    }
+
+    [BindProperty]
+    public string? ErrorMessage { get; set; }
+    [BindProperty]
+    public IFormFile FileSent { get; set; }
+    public List<WorkOrderResumeDTO> Reports { get; set; } = new();
+
+
+    public async Task<IActionResult> OnGetAsync(int page = 0)
+    {
+        try { Reports = await _service.GetResumeAsync(page); }
+        catch (System.Exception ex)
+        {
+            ErrorMessage = ex.Message;
+        }
+        return Page();
+    }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            try
+            {
+                await _service.AddRangeAsync(FileSent.OpenReadStream(), FileSent.FileName);
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+                return Page();
+            }
+            return RedirectToPage("./Index");
+        }
+
+}
