241b5f7d45bb39da0438c0ef147ce90bee0f977a
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-06-26 14:56:05 -0300

    feat: implement DeleteRangeAsync method to handle bulk deletion of employers

--- src/Indica.Productivity.Application/Services/EmployerService.cs
+++ src/Indica.Productivity.Application/Services/EmployerService.cs
@@ -31,5 +31,21 @@ namespace Indica.Productivity.Application.Services
             await _repository.UpdateAsync(employer);
             return true;
         }
+
+        public override async Task<int> DeleteRangeAsync(List<EmployerDTO> employersToDel)
+        {
+            var employers = await _repository.GetAllAsync();
+            var situationID = (await _situationRepository.GetByIdAsync(TERMINATED_CODE)).Id;
+            var deletingIDs = employersToDel.Select(e => e.Id).ToHashSet();
+            employers = employers.Where(e => deletingIDs.Contains(e.Id)).ToList();
+            employers.ForEach(e =>
+            {
+                e.Demission = DateOnly.FromDateTime(DateTime.Now);
+                e.IdSituation = situationID;
+                e.Situation = null;
+                e.Function = null;
+            });
+            return await _repository.UpdateRangeAsync(employers);
+        }
     }
 }
