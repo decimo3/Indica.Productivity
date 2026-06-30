0f150f6432325fd2aab5807bda626183966cfc92
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-06-30 08:43:50 -0300

    feat: initialize StartDate and FinalDate with default values in ContractDTO

--- src/Indica.Productivity.Application/DTO/ContractDTO.cs
+++ src/Indica.Productivity.Application/DTO/ContractDTO.cs
@@ -4,7 +4,7 @@
     {
         public long ContractNumber { get; set; }
         public int AdditiveNumber { get; set; }
-        public DateOnly StartDate { get; set; }
-        public DateOnly FinalDate { get; set; }
+        public DateOnly StartDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        public DateOnly FinalDate { get; set; } = DateOnly.MaxValue;
     }
 }
